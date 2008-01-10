b5f857754eae210ef74e4ea7e8458b1da34ee011
Andrew Kornev <akornev@apache.org>
2008-01-10 01:10:43 +0000

    Made logging operation thread-safe
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/zookeeper/trunk@670865 13f79535-47bb-0310-9956-ffa450edef68

--- zookeeper/c/src/zk_log.h
+++ zookeeper/c/src/zk_log.h
@@ -3,11 +3,15 @@
 
 #include <zookeeper.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 extern ZOOAPI ZooLogLevel logLevel;
 #define LOGSTREAM stderr
 
-#define LOG_ERROR(x) \
-    log_message(LOG_LEVEL_DEBUG,__LINE__,__func__,format_log_message x)
+#define LOG_ERROR(x) if(logLevel>=LOG_LEVEL_ERROR) \
+    log_message(LOG_LEVEL_ERROR,__LINE__,__func__,format_log_message x)
 #define LOG_WARN(x) if(logLevel>=LOG_LEVEL_WARN) \
     log_message(LOG_LEVEL_WARN,__LINE__,__func__,format_log_message x)
 #define LOG_INFO(x) if(logLevel>=LOG_LEVEL_INFO) \
@@ -21,5 +25,9 @@ void log_message(ZooLogLevel curLevel, int line,const char* funcName,
     const char* message);
 
 const char* format_log_message(const char* format,...);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /*ZK_LOG_H_*/
