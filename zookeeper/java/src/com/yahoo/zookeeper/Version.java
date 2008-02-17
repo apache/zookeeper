4b0cc456e9c1b5ffe4bf0c666545c74ffc9664d3
Andrew Kornev <akornev@apache.org>
2008-02-17 01:49:46 +0000

    take three
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/zookeeper/trunk@670896 13f79535-47bb-0310-9956-ffa450edef68

--- zookeeper/java/src/com/yahoo/zookeeper/Version.java
+++ zookeeper/java/src/com/yahoo/zookeeper/Version.java
@@ -1,5 +1,6 @@
 package com.yahoo.zookeeper;
 
 public class Version {
-	public static final String sccs="$Id$";
+	public static final String id="$Id$";
+	public static final String revision="$Revision$";	
 }
