cef8ad10d66a54d9d5969cd8ea1682f58798c316
Andrew Kornev <akornev@apache.org>
2008-02-21 22:19:46 +0000

    Added support for server version info at runtime; added the "release" target to ant build file
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/zookeeper/trunk@670899 13f79535-47bb-0310-9956-ffa450edef68

--- zookeeper/java/src/com/yahoo/zookeeper/Version.java
+++ zookeeper/java/src/com/yahoo/zookeeper/Version.java
@@ -1,6 +1,60 @@
 package com.yahoo.zookeeper;
 
-public class Version {
-	public static final String revisionDate="$Date$";
-	public static final String revision="$Revision$";	
+public class Version implements com.yahoo.zookeeper.version.Info{
+
+	public static int getRevision() {
+		return REVISION;
+	}
+
+	public static String getBuildDate() {
+		return BUILD_DATE;
+	}
+
+	public static String getVersion() {
+		return MAJOR + "." + MINOR + "." + MICRO;
+	}
+
+	public static String getVersionRevision() {
+		return getVersion() + "-" + getRevision();
+	}
+
+	public static String getFullVersion() {
+		return getVersionRevision() + ", built on " + getBuildDate();
+	}
+
+	public static void printUsage() {
+		System.out
+				.print("Usage:\tjava -cp ... com.yahoo.zookeeper.Version "
+						+ "[--full | --short | --revision],\n\tPrints --full version "
+						+ "info if no arg specified.");
+		System.exit(1);
+	}
+
+	/**
+	 * Prints the current version, revision and build date to the standard out.
+	 * 
+	 * @param args
+	 *            <ul>
+	 *            <li> --short - prints a short version string "1.2.3"
+	 *            <li> --revision - prints a short version string with the SVN
+	 *            repository revision "1.2.3-94"
+	 *            <li> --full - prints the revision and the build date
+	 *            </ul>
+	 */
+	public static void main(String[] args) {
+		if (args.length > 1) {
+			printUsage();
+		}
+		if (args.length == 0 || (args.length == 1 && args[0].equals("--full"))) {
+			System.out.println(getFullVersion());
+			System.exit(0);
+		}
+		if (args[0].equals("--short"))
+			System.out.println(getVersion());
+		else if (args[0].equals("--revision"))
+			System.out.println(getVersionRevision());
+		else
+			printUsage();
+		System.exit(0);
+	}
 }
