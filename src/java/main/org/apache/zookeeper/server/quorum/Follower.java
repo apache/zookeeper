b4b73a37d43413efdf50427f96aab2720af11baf
Patrick D. Hunt <phunt@apache.org>
2008-06-24 18:56:38 +0000

    Edited source files to move from Yahoo to Apache source license header. See ZOOKEEPER-45
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/zookeeper/trunk@671295 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/main/org/apache/zookeeper/server/quorum/Follower.java
+++ src/java/main/org/apache/zookeeper/server/quorum/Follower.java
@@ -1,9 +1,11 @@
-/*
- * Copyright 2008, Yahoo! Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -13,9 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/**
- * This class has the control logic for the Follower.
- */
+
 package org.apache.zookeeper.server.quorum;
 
 import java.io.BufferedInputStream;
@@ -46,6 +46,9 @@ import org.apache.zookeeper.server.ZooTrace;
 import org.apache.zookeeper.server.quorum.QuorumPeer.QuorumServer;
 import org.apache.zookeeper.txn.TxnHeader;
 
+/**
+ * This class has the control logic for the Follower.
+ */
 public class Follower {
     private static final Logger LOG = Logger.getLogger(Follower.class);
 
