63f1fa23c25da97fa9020e1695a924e665bbba3d
Mahadev Konar <mahadev@apache.org>
2009-08-03 21:32:08 +0000

    ZOOKEEPER-481. Add lastMessageSent to QuorumCnxManager. (flavio via mahadev)
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/zookeeper/branches/branch-3.2@800576 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/test/org/apache/zookeeper/test/CnxManagerTest.java
@@ -0,0 +1,157 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.zookeeper.test;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.concurrent.Semaphore;
+
+import junit.framework.TestCase;
+
+import org.apache.log4j.Logger;
+import org.apache.zookeeper.server.quorum.FastLeaderElection;
+import org.apache.zookeeper.server.quorum.QuorumCnxManager;
+import org.apache.zookeeper.server.quorum.QuorumPeer;
+import org.apache.zookeeper.server.quorum.Vote;
+import org.apache.zookeeper.server.quorum.QuorumPeer.QuorumServer;
+import org.apache.zookeeper.server.quorum.QuorumPeer.ServerState;
+import org.junit.Test;
+
+
+/**
+ * This test uses two mock servers, each running an instance of QuorumCnxManager.
+ * It simulates the situation in which a peer P sends a message to another peer Q 
+ * while Q is trying to open a connection to P. In this test, Q iniates a connection
+ * to P as soon as it receives a message from P, and verifies that it receives a 
+ * copy of the message.
+ * 
+ * This simple tests verifies that the new mechanism that duplicates the last message
+ * sent upon a re-connection works. 
+ *
+ */
+public class CnxManagerTest extends TestCase {
+    protected static final Logger LOG = Logger.getLogger(FLENewEpochTest.class);
+    
+    int baseport;
+    int baseLEport;
+    int count;
+    HashMap<Long,QuorumServer> peers;
+    File tmpdir[];
+    int port[];
+    
+    public void setUp() throws Exception {
+        this.baseport= 33003;
+        this.baseLEport = 43003;
+        this.count = 3;
+        this.peers = new HashMap<Long,QuorumServer>(count); 
+        tmpdir = new File[count];
+        port = new int[count];
+        
+        for(int i = 0; i < count; i++) {
+            peers.put(Long.valueOf(i),
+                    new QuorumServer(i,
+                            new InetSocketAddress(baseport + i),
+                    new InetSocketAddress(baseLEport + i)));
+            tmpdir[i] = ClientBase.createTmpDir();
+            port[i] = baseport + i;
+        }
+    }
+    
+    public void tearDown() {
+        
+    }
+    
+    
+    ByteBuffer createMsg(int state, long leader, long zxid, long epoch){
+        byte requestBytes[] = new byte[28];
+        ByteBuffer requestBuffer = ByteBuffer.wrap(requestBytes);  
+        
+        /*
+         * Building notification packet to send
+         */
+                
+        requestBuffer.clear();
+        requestBuffer.putInt(state);
+        requestBuffer.putLong(leader);
+        requestBuffer.putLong(zxid);
+        requestBuffer.putLong(epoch);
+        
+        return requestBuffer;
+    }
+    
+    class CnxManagerThread extends Thread {
+        
+        CnxManagerThread(){}
+        
+        public void run(){
+            try {
+                QuorumPeer peer = new QuorumPeer(peers, tmpdir[0], tmpdir[0], port[0], 3, 0, 2, 2, 2);
+                QuorumCnxManager cnxManager = new QuorumCnxManager(peer);
+                QuorumCnxManager.Listener listener = cnxManager.listener;
+                if(listener != null){
+                    listener.start();
+                } else {
+                    LOG.error("Null listener when initializing cnx manager");
+                }
+                
+                long sid = 1;
+                cnxManager.toSend(sid, createMsg(ServerState.LOOKING.ordinal(), 0, -1, 1));
+                cnxManager.recvQueue.take();
+                cnxManager.testInitiateConnection(sid);
+            
+                cnxManager.recvQueue.take();
+            } catch (Exception e) {
+                LOG.error("Exception while running mock thread", e);
+                fail("Unexpected exception");
+            }
+        }
+    }
+    
+    @Test
+    public void testCnxManager() throws Exception {
+        Thread thread = new CnxManagerThread();
+        
+        thread.start();
+        
+        QuorumPeer peer = new QuorumPeer(peers, tmpdir[1], tmpdir[1], port[1], 3, 1, 2, 2, 2);
+        QuorumCnxManager cnxManager = new QuorumCnxManager(peer);
+        QuorumCnxManager.Listener listener = cnxManager.listener;
+        if(listener != null){
+            listener.start();
+        } else {
+            LOG.error("Null listener when initializing cnx manager");
+        }
+            
+        cnxManager.toSend(new Long(0), createMsg(ServerState.LOOKING.ordinal(), 1, -1, 1));
+        cnxManager.recvQueue.take();
+        
+        thread.join(5000);
+        if (thread.isAlive()) {
+            fail("Threads didn't join");
+        }
+    }
+    
+    
+    
+}
