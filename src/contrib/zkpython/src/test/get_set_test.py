1a0f577000d8e90b0f92a30a69bff6d1ec581a84
Mahadev Konar <mahadev@apache.org>
2009-10-09 00:42:18 +0000

    ZOOKEEPER-510. zkpython lumps all exceptions as IOError, needs specialized exceptions for KeeperException types (henry & pat via mahadev)
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/zookeeper/trunk@823371 13f79535-47bb-0310-9956-ffa450edef68

--- src/contrib/zkpython/src/test/get_set_test.py
+++ src/contrib/zkpython/src/test/get_set_test.py
@@ -36,17 +36,23 @@ class GetSetTest(zktestbase.TestBase):
         ret = zookeeper.set(self.handle, "/zk-python-getsettest",
                             "off", stat["version"])
         (data,stat) = zookeeper.get(self.handle, "/zk-python-getsettest", None)
-        self.assertEqual(data, "off", "Data is not 'off' as expected: " + data)        
+        self.assertEqual(data, "off", "Data is not 'off' as expected: " + data)
+        self.assertRaises(zookeeper.BadVersionException,
+                          zookeeper.set,
+                          self.handle,
+                          "/zk-python-getsettest",
+                          "test",
+                          stat["version"]+1)
 
     def test_async_getset(self):
-        self.cv = threading.Condition()        
+        self.cv = threading.Condition()
         def get_callback(handle, rc, value, stat):
             self.cv.acquire()
             self.callback_flag = True
             self.rc = rc
             self.value = (value,stat)
             self.cv.notify()
-            self.cv.release()            
+            self.cv.release()
 
         def set_callback(handle, rc, stat):
             self.cv.acquire()
@@ -56,8 +62,8 @@ class GetSetTest(zktestbase.TestBase):
             self.cv.notify()
             self.cv.release()
 
-        self.assertEqual(self.connected, True, "Not connected!")            
-        
+        self.assertEqual(self.connected, True, "Not connected!")
+
         self.cv.acquire()
         self.callback_flag = False
         ret = zookeeper.aset(self.handle, "/zk-python-agetsettest", "off", -1, set_callback)
@@ -66,16 +72,42 @@ class GetSetTest(zktestbase.TestBase):
             self.cv.wait(15)
         self.cv.release()
         self.assertEqual(self.callback_flag, True, "aset timed out")
-        
+
         self.cv.acquire()
         self.callback_flag = False
-        ret = zookeeper.aget(self.handle, "/zk-python-agetsettest", None, get_callback)        
+        ret = zookeeper.aget(self.handle, "/zk-python-agetsettest", None, get_callback)
         self.assertEqual(ret, zookeeper.OK, "aget failed")
-        while not self.callback_flag:
-            self.cv.wait(15)
+        self.cv.wait(15)
         self.cv.release()
         self.assertEqual(self.callback_flag, True, "aget timed out")
         self.assertEqual(self.value[0], "off", "Data is not 'off' as expected: " + self.value[0])
-        
+
+    def test_sync_getchildren(self):
+        self.ensureCreated("/zk-python-getchildrentest", flags=0)
+        self.ensureCreated("/zk-python-getchildrentest/child")
+        children = zookeeper.get_children(self.handle, "/zk-python-getchildrentest")
+        self.assertEqual(len(children), 1, "Expected to find 1 child, got " + str(len(children)))
+
+    def test_async_getchildren(self):
+        self.ensureCreated("/zk-python-getchildrentest", flags=0)
+        self.ensureCreated("/zk-python-getchildrentest/child")
+
+        def gc_callback(handle, rc, children):
+            self.cv.acquire()
+            self.rc = rc
+            self.children = children
+            self.callback_flag = True
+            self.cv.notify()
+            self.cv.release()
+
+        self.cv.acquire()
+        self.callback_flag = False
+        zookeeper.aget_children(self.handle, "/zk-python-getchildrentest", None, gc_callback)
+        self.cv.wait(15)
+        self.assertEqual(self.callback_flag, True, "aget_children timed out")
+        self.assertEqual(self.rc, zookeeper.OK, "Return code for aget_children was not OK - %s" % zookeeper.zerror(self.rc))
+        self.assertEqual(len(self.children), 1, "Expected to find 1 child, got " + str(len(self.children)))
+
+
 if __name__ == '__main__':
     unittest.main()
