47c54fede374c4d45fe408267ffcce8991170d4f
Henry Robinson <henry@apache.org>
2010-04-15 07:23:44 +0000

    ZOOKEEPER-729. Java client API to recursively delete a subtree. (Kay Kay via henry)
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/zookeeper/trunk@934312 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/main/org/apache/zookeeper/ZooKeeperMain.java
+++ src/java/main/org/apache/zookeeper/ZooKeeperMain.java
@@ -65,6 +65,7 @@ public class ZooKeeperMain {
         commandMap.put("close","");
         commandMap.put("create", "[-s] [-e] path data acl");
         commandMap.put("delete","path [version]");
+        commandMap.put("rmr","path");
         commandMap.put("set","path data [version]");
         commandMap.put("get","path [watch]");
         commandMap.put("ls","path [watch]");
@@ -681,6 +682,9 @@ public class ZooKeeperMain {
         } else if (cmd.equals("delete") && args.length >= 2) {
             path = args[1];
             zk.delete(path, watch ? Integer.parseInt(args[2]) : -1);
+        } else if (cmd.equals("rmr") && args.length >= 2) {
+            path = args[1];
+            zk.deleteRecursive(path);
         } else if (cmd.equals("set") && args.length >= 3) {
             path = args[1];
             stat = zk.setData(path, args[2].getBytes(),
