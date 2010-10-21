519a45264453bf788a166d99e276869ced913daa
Mahadev Konar <mahadev@apache.org>
2010-10-21 22:12:50 +0000

    ZOOKEEPER-800. zoo_add_auth returns ZOK if zookeeper handle is in ZOO_CLOSED_STATE (michi mutsuzaki via mahadev konar)
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/zookeeper/trunk@1026159 13f79535-47bb-0310-9956-ffa450edef68

--- src/c/src/zookeeper.c
+++ src/c/src/zookeeper.c
@@ -2934,6 +2934,12 @@ int zoo_add_auth(zhandle_t *zh,const char* scheme,const char* cert,
     if (is_unrecoverable(zh))
         return ZINVALIDSTATE;
 
+    // [ZOOKEEPER-800] zoo_add_auth should return ZINVALIDSTATE if
+    // the connection is closed. 
+    if (zoo_state(zh) == 0) {
+        return ZINVALIDSTATE;
+    }
+
     if(cert!=NULL && certLen!=0){
         auth.buff=calloc(1,certLen);
         if(auth.buff==0) {
