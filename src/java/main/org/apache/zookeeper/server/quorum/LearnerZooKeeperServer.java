01cd40b9a03b832007176a087958ec3325998020
Mahadev Konar <mahadev@apache.org>
2012-01-31 06:54:55 +0000

    ZOOKEEPER-1367. Data inconsistencies and unexpired ephemeral nodes after cluster restart. (Benjamin Reed via mahadev)
    git-svn-id: https://svn.apache.org/repos/asf/zookeeper/branches/branch-3.3@1238178 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/main/org/apache/zookeeper/server/quorum/LearnerZooKeeperServer.java
+++ src/java/main/org/apache/zookeeper/server/quorum/LearnerZooKeeperServer.java
@@ -70,10 +70,13 @@ public abstract class LearnerZooKeeperServer extends QuorumZooKeeperServer {
     }    
     
     @Override
-    protected void createSessionTracker() {
+    public void createSessionTracker() {
         sessionTracker = new LearnerSessionTracker(this, getZKDatabase().getSessionWithTimeOuts(),
                 self.getId());
     }
+    
+    @Override
+    protected void startSessionTracker() {}
     
     @Override
     protected void revalidateSession(ServerCnxn cnxn, long sessionId,
