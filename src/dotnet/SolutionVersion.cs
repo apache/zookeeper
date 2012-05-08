f18ceb59af3a0f861ba1c3c5da470c815d020d5d
omwok <legowo.budianto@google.com>
2012-05-08 18:49:40 +0700

    add missing file

---
+++ src/dotnet/SolutionVersion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security;
+[assembly: AssemblyDescription("ZooKeeper Client for .NET http://zookeeper.apache.org")]
+[assembly: AssemblyProduct("ZooKeeperNet")]
+[assembly: AssemblyCopyright("Copyright 2007-2012 ewhauser, omwok. - All rights reserved.")]
+[assembly: AssemblyVersion("0.3.0")]
+[assembly: AssemblyFileVersion("0.3.0")]
+
+[assembly: AssemblyInformationalVersion("0.3.0.343")]
+[assembly: ComVisibleAttribute(false)]
+[assembly: CLSCompliantAttribute(false)]
+
