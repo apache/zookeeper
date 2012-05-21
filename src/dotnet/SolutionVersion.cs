719892f16668ac107b391a25691da83a6c1bbe36
omwok <legowo.budianto@google.com>
2012-05-21 15:56:23 +0700

    some profile files and solution version

--- src/dotnet/SolutionVersion.cs
+++ src/dotnet/SolutionVersion.cs
@@ -5,10 +5,10 @@ using System.Security;
 [assembly: AssemblyDescription("ZooKeeper Client for .NET http://zookeeper.apache.org")]
 [assembly: AssemblyProduct("ZooKeeperNet")]
 [assembly: AssemblyCopyright("Copyright 2007-2012 ewhauser, omwok. - All rights reserved.")]
-[assembly: AssemblyVersion("0.4.2")]
-[assembly: AssemblyFileVersion("0.4.2")]
+[assembly: AssemblyVersion("0.4.3")]
+[assembly: AssemblyFileVersion("0.4.3")]
 
-[assembly: AssemblyInformationalVersion("0.4.2.343")]
+[assembly: AssemblyInformationalVersion("0.4.3.343")]
 [assembly: ComVisibleAttribute(false)]
 [assembly: CLSCompliantAttribute(false)]
 
