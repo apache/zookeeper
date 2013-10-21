d4182fac6aec8bc169daecb0fa68f853129abcab
Flavio Paiva Junqueira <fpj@apache.org>
2013-10-21 21:36:55 +0000

    ZOOKEEPER-1732. ZooKeeper server unable to join established ensemble (German Blanco via fpj)
    git-svn-id: https://svn.apache.org/repos/asf/zookeeper/trunk@1534390 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/main/org/apache/zookeeper/server/quorum/Leader.java
+++ src/java/main/org/apache/zookeeper/server/quorum/Leader.java
@@ -1242,6 +1242,15 @@ public class Leader {
         }
         
         zk.startup();
+        /*
+         * Update the election vote here to ensure that all members of the
+         * ensemble report the same vote to new servers that start up and
+         * send leader election notifications to the ensemble.
+         * 
+         * @see https://issues.apache.org/jira/browse/ZOOKEEPER-1732
+         */
+        self.updateElectionVote(getEpoch());
+
         zk.getZKDatabase().setlastProcessedZxid(zk.getZxid());
     }
 
