3dae9c4770f77e28fa6bba07e58133f1530f381a
Thawan Kooburat <thawan@apache.org>
2013-10-24 18:52:31 +0000

    ZOOKEEPER-1798. Fix race condition in testNormalObserverRun (thawan, fpj via thawan)
    git-svn-id: https://svn.apache.org/repos/asf/zookeeper/branches/branch-3.4@1535497 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/main/org/apache/zookeeper/server/SyncRequestProcessor.java
+++ src/java/main/org/apache/zookeeper/server/SyncRequestProcessor.java
@@ -212,8 +212,15 @@ public class SyncRequestProcessor extends Thread implements RequestProcessor {
             if(running){
                 this.join();
             }
+            if (!toFlush.isEmpty()) {
+                flush(toFlush);
+            }
         } catch(InterruptedException e) {
             LOG.warn("Interrupted while wating for " + this + " to finish");
+        } catch (IOException e) {
+            LOG.warn("Got IO exception during shutdown");
+        } catch (RequestProcessorException e) {
+            LOG.warn("Got request processor exception during shutdown");
         }
         if (nextProcessor != null) {
             nextProcessor.shutdown();
