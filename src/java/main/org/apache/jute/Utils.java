a7567ccddd6849409efa7c3a164bff6c1bcf0af6
Flavio Paiva Junqueira <fpj@apache.org>
2014-09-18 15:51:08 +0000

    ZOOKEEPER-2039. Jute compareBytes incorrect comparison index (Ian Dimayuga via fpj)
    git-svn-id: https://svn.apache.org/repos/asf/zookeeper/branches/branch-3.4@1626009 13f79535-47bb-0310-9956-ffa450edef68
    
    Conflicts:
    	CHANGES.txt

--- src/java/main/org/apache/jute/Utils.java
+++ src/java/main/org/apache/jute/Utils.java
@@ -268,15 +268,15 @@ public class Utils {
         return stream.toByteArray();
     }
     public static int compareBytes(byte b1[], int off1, int len1, byte b2[], int off2, int len2) {
-    	int i;
-    	for(i=0; i < len1 && i < len2; i++) {
-    		if (b1[off1+i] != b2[off2+i]) {
-    			return b1[off1+i] < b2[off2+1] ? -1 : 1;
-    		}
-    	}
-    	if (len1 != len2) {
-    		return len1 < len2 ? -1 : 1;
-    	}
-    	return 0;
+        int i;
+        for(i=0; i < len1 && i < len2; i++) {
+            if (b1[off1+i] != b2[off2+i]) {
+                return b1[off1+i] < b2[off2+i] ? -1 : 1;
+            }
+        }
+        if (len1 != len2) {
+            return len1 < len2 ? -1 : 1;
+        }
+        return 0;
     }
 }
