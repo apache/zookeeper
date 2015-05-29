307349001833bc45fc13d1fed177c5dc504c5048
Raúl Gutiérrez Segalés <rgs@apache.org>
2015-05-29 19:48:23 +0000

    ZOOKEEPER-2179: Typo in Watcher.java (Archana T via rgs)
    git-svn-id: https://svn.apache.org/repos/asf/zookeeper/trunk@1682539 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/main/org/apache/zookeeper/Watcher.java
+++ src/java/main/org/apache/zookeeper/Watcher.java
@@ -20,7 +20,7 @@ package org.apache.zookeeper;
 
 /**
  * This interface specifies the public interface an event handler class must
- * implement. A ZooKeeper client will get various events from the ZooKeepr
+ * implement. A ZooKeeper client will get various events from the ZooKeeper
  * server it connects to. An application using such a client handles these
  * events by registering a callback object with the client. The callback object
  * is expected to be an instance of a class that implements Watcher interface.
