3fd77b419673ce6ec41e06cdc27558b1d8f4ca06
Raúl Gutiérrez Segalés <rgs@apache.org>
2015-06-28 19:09:49 +0000

    ZOOKEEPER-2140: NettyServerCnxn and NIOServerCnxn code should be improved (Arshad Mohammad via rgs)
    git-svn-id: https://svn.apache.org/repos/asf/zookeeper/trunk@1688041 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/main/org/apache/zookeeper/server/NIOServerCnxn.java
+++ src/java/main/org/apache/zookeeper/server/NIOServerCnxn.java
@@ -30,29 +30,25 @@ import java.nio.channels.CancelledKeyException;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.SocketChannel;
 import java.security.cert.Certificate;
-import java.util.List;
 import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.LinkedBlockingQueue;
 
 import org.apache.jute.BinaryInputArchive;
 import org.apache.jute.BinaryOutputArchive;
 import org.apache.jute.Record;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.apache.zookeeper.Environment;
-import org.apache.zookeeper.Version;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.data.Id;
 import org.apache.zookeeper.proto.ReplyHeader;
 import org.apache.zookeeper.proto.RequestHeader;
 import org.apache.zookeeper.proto.WatcherEvent;
 import org.apache.zookeeper.server.NIOServerCnxnFactory.SelectorThread;
-import org.apache.zookeeper.server.quorum.Leader;
-import org.apache.zookeeper.server.quorum.LeaderZooKeeperServer;
-import org.apache.zookeeper.server.quorum.ReadOnlyZooKeeperServer;
-import org.apache.zookeeper.server.util.OSMXBean;
+import org.apache.zookeeper.server.command.CommandExecutor;
+import org.apache.zookeeper.server.command.FourLetterCommands;
+import org.apache.zookeeper.server.command.SetTraceMaskCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * This class handles communication with clients using NIO. There is one per
@@ -438,30 +434,6 @@ public class NIOServerCnxn extends ServerCnxn {
     }
 
     /**
-     * clean up the socket related to a command and also make sure we flush the
-     * data before we do that
-     *
-     * @param pwriter
-     *            the pwriter for a command socket
-     */
-    private void cleanupWriterSocket(PrintWriter pwriter) {
-        try {
-            if (pwriter != null) {
-                pwriter.flush();
-                pwriter.close();
-            }
-        } catch (Exception e) {
-            LOG.info("Error closing PrintWriter ", e);
-        } finally {
-            try {
-                close();
-            } catch (Exception e) {
-                LOG.error("Error closing a command socket ", e);
-            }
-        }
-    }
-
-    /**
      * This class wraps the sendBuffer method of NIOServerCnxn. It is
      * responsible for chunking up the response to a client. Rather
      * than cons'ing up a response fully in memory, which may be large
@@ -500,336 +472,13 @@ public class NIOServerCnxn extends ServerCnxn {
             checkFlush(false);
         }
     }
-
-    private static final String ZK_NOT_SERVING =
-        "This ZooKeeper instance is not currently serving requests";
-
-    /**
-     * Set of threads for commmand ports. All the 4
-     * letter commands are run via a thread. Each class
-     * maps to a corresponding 4 letter command. CommandThread
-     * is the abstract class from which all the others inherit.
-     */
-    private abstract class CommandThread {
-        PrintWriter pw;
-
-        CommandThread(PrintWriter pw) {
-            this.pw = pw;
-        }
-
-        public void start() {
-            run();
-        }
-
-        public void run() {
-            try {
-                commandRun();
-            } catch (IOException ie) {
-                LOG.error("Error in running command ", ie);
-            } finally {
-                cleanupWriterSocket(pw);
-            }
-        }
-
-        public abstract void commandRun() throws IOException;
-    }
-
-    private class RuokCommand extends CommandThread {
-        public RuokCommand(PrintWriter pw) {
-            super(pw);
-        }
-
-        @Override
-        public void commandRun() {
-            pw.print("imok");
-
-        }
-    }
-
-    private class TraceMaskCommand extends CommandThread {
-        TraceMaskCommand(PrintWriter pw) {
-            super(pw);
-        }
-
-        @Override
-        public void commandRun() {
-            long traceMask = ZooTrace.getTextTraceLevel();
-            pw.print(traceMask);
-        }
-    }
-
-    private class SetTraceMaskCommand extends CommandThread {
-        long trace = 0;
-        SetTraceMaskCommand(PrintWriter pw, long trace) {
-            super(pw);
-            this.trace = trace;
-        }
-
-        @Override
-        public void commandRun() {
-            pw.print(trace);
-        }
-    }
-
-    private class EnvCommand extends CommandThread {
-        EnvCommand(PrintWriter pw) {
-            super(pw);
-        }
-
-        @Override
-        public void commandRun() {
-            List<Environment.Entry> env = Environment.list();
-
-            pw.println("Environment:");
-            for(Environment.Entry e : env) {
-                pw.print(e.getKey());
-                pw.print("=");
-                pw.println(e.getValue());
-            }
-
-        }
-    }
-
-    private class ConfCommand extends CommandThread {
-        ConfCommand(PrintWriter pw) {
-            super(pw);
-        }
-
-        @Override
-        public void commandRun() {
-            if (zkServer == null) {
-                pw.println(ZK_NOT_SERVING);
-            } else {
-                zkServer.dumpConf(pw);
-            }
-        }
-    }
-
-    private class StatResetCommand extends CommandThread {
-        public StatResetCommand(PrintWriter pw) {
-            super(pw);
-        }
-
-        @Override
-        public void commandRun() {
-            if (zkServer == null) {
-                pw.println(ZK_NOT_SERVING);
-            }
-            else {
-                zkServer.serverStats().reset();
-                pw.println("Server stats reset.");
-            }
-        }
-    }
-
-    private class CnxnStatResetCommand extends CommandThread {
-        public CnxnStatResetCommand(PrintWriter pw) {
-            super(pw);
-        }
-
-        @Override
-        public void commandRun() {
-            if (zkServer == null) {
-                pw.println(ZK_NOT_SERVING);
-            } else {
-                for(ServerCnxn c : factory.cnxns){
-                    c.resetStats();
-                }
-                pw.println("Connection stats reset.");
-            }
-        }
-    }
-
-    private class DumpCommand extends CommandThread {
-        public DumpCommand(PrintWriter pw) {
-            super(pw);
-        }
-
-        @Override
-        public void commandRun() {
-            if (zkServer == null) {
-                pw.println(ZK_NOT_SERVING);
-            }
-            else {
-                pw.println("SessionTracker dump:");
-                zkServer.sessionTracker.dumpSessions(pw);
-                pw.println("ephemeral nodes dump:");
-                zkServer.dumpEphemerals(pw);
-                pw.println("Connections dump:");
-                factory.dumpConnections(pw);
-            }
-        }
-    }
-
-    private class StatCommand extends CommandThread {
-        int len;
-        public StatCommand(PrintWriter pw, int len) {
-            super(pw);
-            this.len = len;
-        }
-
-        @SuppressWarnings("unchecked")
-        @Override
-        public void commandRun() {
-            if (zkServer == null) {
-                pw.println(ZK_NOT_SERVING);
-            }
-            else {
-                pw.print("Zookeeper version: ");
-                pw.println(Version.getFullVersion());
-                if (zkServer instanceof ReadOnlyZooKeeperServer) {
-                    pw.println("READ-ONLY mode; serving only " +
-                               "read-only clients");
-                }
-                if (len == statCmd) {
-                    LOG.info("Stat command output");
-                    pw.println("Clients:");
-                    for(ServerCnxn c : factory.cnxns){
-                        c.dumpConnectionInfo(pw, true);
-                        pw.println();
-                    }
-                    pw.println();
-                }
-                pw.print(zkServer.serverStats().toString());
-                pw.print("Node count: ");
-                pw.println(zkServer.getZKDatabase().getNodeCount());
-            }
-
-        }
-    }
-
-    private class ConsCommand extends CommandThread {
-        public ConsCommand(PrintWriter pw) {
-            super(pw);
-        }
-
-        @SuppressWarnings("unchecked")
-        @Override
-        public void commandRun() {
-            if (zkServer == null) {
-                pw.println(ZK_NOT_SERVING);
-            } else {
-                for (ServerCnxn c : factory.cnxns) {
-                    c.dumpConnectionInfo(pw, false);
-                    pw.println();
-                }
-                pw.println();
-            }
-        }
-    }
-
-    private class WatchCommand extends CommandThread {
-        int len = 0;
-        public WatchCommand(PrintWriter pw, int len) {
-            super(pw);
-            this.len = len;
-        }
-
-        @Override
-        public void commandRun() {
-            if (zkServer == null) {
-                pw.println(ZK_NOT_SERVING);
-            } else {
-                DataTree dt = zkServer.getZKDatabase().getDataTree();
-                if (len == wchsCmd) {
-                    dt.dumpWatchesSummary(pw);
-                } else if (len == wchpCmd) {
-                    dt.dumpWatches(pw, true);
-                } else {
-                    dt.dumpWatches(pw, false);
-                }
-                pw.println();
-            }
-        }
-    }
-
-    private class MonitorCommand extends CommandThread {
-
-        MonitorCommand(PrintWriter pw) {
-            super(pw);
-        }
-
-        @Override
-        public void commandRun() {
-            if(zkServer == null) {
-                pw.println(ZK_NOT_SERVING);
-                return;
-            }
-            ZKDatabase zkdb = zkServer.getZKDatabase();
-            ServerStats stats = zkServer.serverStats();
-
-            print("version", Version.getFullVersion());
-
-            print("avg_latency", stats.getAvgLatency());
-            print("max_latency", stats.getMaxLatency());
-            print("min_latency", stats.getMinLatency());
-
-            print("packets_received", stats.getPacketsReceived());
-            print("packets_sent", stats.getPacketsSent());
-            print("num_alive_connections", stats.getNumAliveClientConnections());
-
-            print("outstanding_requests", stats.getOutstandingRequests());
-
-            print("server_state", stats.getServerState());
-            print("znode_count", zkdb.getNodeCount());
-
-            print("watch_count", zkdb.getDataTree().getWatchCount());
-            print("ephemerals_count", zkdb.getDataTree().getEphemeralsCount());
-            print("approximate_data_size", zkdb.getDataTree().approximateDataSize());
-
-            OSMXBean osMbean = new OSMXBean();
-            if (osMbean != null && osMbean.getUnix() == true) {
-                print("open_file_descriptor_count", osMbean.getOpenFileDescriptorCount());
-                print("max_file_descriptor_count", osMbean.getMaxFileDescriptorCount());
-            }
-
-            if(stats.getServerState().equals("leader")) {
-                Leader leader = ((LeaderZooKeeperServer)zkServer).getLeader();
-
-                print("followers", leader.getLearners().size());
-                print("synced_followers", leader.getForwardingFollowers().size());
-                print("pending_syncs", leader.getNumPendingSyncs());
-            }
-        }
-
-        private void print(String key, long number) {
-            print(key, "" + number);
-        }
-
-        private void print(String key, String value) {
-            pw.print("zk_");
-            pw.print(key);
-            pw.print("\t");
-            pw.println(value);
-        }
-
-    }
-
-    private class IsroCommand extends CommandThread {
-
-        public IsroCommand(PrintWriter pw) {
-            super(pw);
-        }
-
-        @Override
-        public void commandRun() {
-            if (zkServer == null) {
-                pw.print("null");
-            } else if (zkServer instanceof ReadOnlyZooKeeperServer) {
-                pw.print("ro");
-            } else {
-                pw.print("rw");
-            }
-        }
-    }
-
     /** Return if four letter word found and responded to, otw false **/
     private boolean checkFourLetterWord(final SelectionKey k, final int len)
     throws IOException
     {
         // We take advantage of the limited size of the length to look
         // for cmds. They are all 4-bytes which fits inside of an int
-        String cmd = cmd2String.get(len);
+        String cmd = FourLetterCommands.cmd2String.get(len);
         if (cmd == null) {
             return false;
         }
@@ -856,71 +505,21 @@ public class NIOServerCnxn extends ServerCnxn {
 
         final PrintWriter pwriter = new PrintWriter(
                 new BufferedWriter(new SendBufferWriter()));
-        if (len == ruokCmd) {
-            RuokCommand ruok = new RuokCommand(pwriter);
-            ruok.start();
-            return true;
-        } else if (len == getTraceMaskCmd) {
-            TraceMaskCommand tmask = new TraceMaskCommand(pwriter);
-            tmask.start();
-            return true;
-        } else if (len == setTraceMaskCmd) {
+        if (len == FourLetterCommands.setTraceMaskCmd) {
             int rc = sock.read(incomingBuffer);
             if (rc < 0) {
                 throw new IOException("Read error");
             }
-
             incomingBuffer.flip();
             long traceMask = incomingBuffer.getLong();
             ZooTrace.setTextTraceLevel(traceMask);
-            SetTraceMaskCommand setMask = new SetTraceMaskCommand(pwriter, traceMask);
+            SetTraceMaskCommand setMask = new SetTraceMaskCommand(pwriter, this, traceMask);
             setMask.start();
             return true;
-        } else if (len == enviCmd) {
-            EnvCommand env = new EnvCommand(pwriter);
-            env.start();
-            return true;
-        } else if (len == confCmd) {
-            ConfCommand ccmd = new ConfCommand(pwriter);
-            ccmd.start();
-            return true;
-        } else if (len == srstCmd) {
-            StatResetCommand strst = new StatResetCommand(pwriter);
-            strst.start();
-            return true;
-        } else if (len == crstCmd) {
-            CnxnStatResetCommand crst = new CnxnStatResetCommand(pwriter);
-            crst.start();
-            return true;
-        } else if (len == dumpCmd) {
-            DumpCommand dump = new DumpCommand(pwriter);
-            dump.start();
-            return true;
-        } else if (len == statCmd || len == srvrCmd) {
-            StatCommand stat = new StatCommand(pwriter, len);
-            stat.start();
-            return true;
-        } else if (len == consCmd) {
-            ConsCommand cons = new ConsCommand(pwriter);
-            cons.start();
-            return true;
-        } else if (len == wchpCmd || len == wchcCmd || len == wchsCmd) {
-            WatchCommand wcmd = new WatchCommand(pwriter, len);
-            wcmd.start();
-            return true;
-        } else if (len == mntrCmd) {
-            MonitorCommand mntr = new MonitorCommand(pwriter);
-            mntr.start();
-            return true;
-        } else if (len == isroCmd) {
-            IsroCommand isro = new IsroCommand(pwriter);
-            isro.start();
-            return true;
-        } else if (len == telnetCloseCmd) {
-            cleanupWriterSocket(null);
-            return true;
+        } else {
+            CommandExecutor commandExecutor = new CommandExecutor();
+            return commandExecutor.execute(this, pwriter, len, zkServer, factory);
         }
-        return false;
     }
 
     /** Reads the first 4 bytes of lenBuffer, which could be true length or
@@ -1195,4 +794,5 @@ public class NIOServerCnxn extends ServerCnxn {
         throw new UnsupportedOperationException(
                 "SSL is unsupported in NIOServerCnxn");
     }
+
 }
