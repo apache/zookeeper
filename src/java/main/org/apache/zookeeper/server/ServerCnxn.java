d351719f141254a9b09ef1d1364c52695e18b4fc
Raúl Gutiérrez Segalés <rgs@apache.org>
2015-06-29 15:32:23 +0000

    ZOOKEEPER-2140: NettyServerCnxn and NIOServerCnxn code should be improved (Arshad Mohammad via rgs)
    git-svn-id: https://svn.apache.org/repos/asf/zookeeper/branches/branch-3.5@1688252 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/main/org/apache/zookeeper/server/ServerCnxn.java
+++ src/java/main/org/apache/zookeeper/server/ServerCnxn.java
@@ -27,7 +27,6 @@ import java.security.cert.Certificate;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -39,6 +38,8 @@ import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.data.Id;
 import org.apache.zookeeper.proto.ReplyHeader;
 import org.apache.zookeeper.proto.RequestHeader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Interface to a Server connection - represents a connection from a client
@@ -48,6 +49,7 @@ public abstract class ServerCnxn implements Stats, Watcher {
     // This is just an arbitrary object to represent requests issued by
     // (aka owned by) this class
     final public static Object me = new Object();
+    private static final Logger LOG = LoggerFactory.getLogger(ServerCnxn.class);
     
     protected ArrayList<Id> authInfo = new ArrayList<Id>();
 
@@ -117,149 +119,6 @@ public abstract class ServerCnxn implements Stats, Watcher {
         public String toString() {
             return "EndOfStreamException: " + getMessage();
         }
-    }
-
-    /*
-     * See <a href="{@docRoot}/../../../docs/zookeeperAdmin.html#sc_zkCommands">
-     * Zk Admin</a>. this link is for all the commands.
-     */
-    protected final static int confCmd =
-        ByteBuffer.wrap("conf".getBytes()).getInt();
-
-    /*
-     * See <a href="{@docRoot}/../../../docs/zookeeperAdmin.html#sc_zkCommands">
-     * Zk Admin</a>. this link is for all the commands.
-     */
-    protected final static int consCmd =
-        ByteBuffer.wrap("cons".getBytes()).getInt();
-
-    /*
-     * See <a href="{@docRoot}/../../../docs/zookeeperAdmin.html#sc_zkCommands">
-     * Zk Admin</a>. this link is for all the commands.
-     */
-    protected final static int crstCmd =
-        ByteBuffer.wrap("crst".getBytes()).getInt();
-
-    /*
-     * See <a href="{@docRoot}/../../../docs/zookeeperAdmin.html#sc_zkCommands">
-     * Zk Admin</a>. this link is for all the commands.
-     */
-    protected final static int dumpCmd =
-        ByteBuffer.wrap("dump".getBytes()).getInt();
-
-    /*
-     * See <a href="{@docRoot}/../../../docs/zookeeperAdmin.html#sc_zkCommands">
-     * Zk Admin</a>. this link is for all the commands.
-     */
-    protected final static int enviCmd =
-        ByteBuffer.wrap("envi".getBytes()).getInt();
-
-    /*
-     * See <a href="{@docRoot}/../../../docs/zookeeperAdmin.html#sc_zkCommands">
-     * Zk Admin</a>. this link is for all the commands.
-     */
-    protected final static int getTraceMaskCmd =
-        ByteBuffer.wrap("gtmk".getBytes()).getInt();
-
-    /*
-     * See <a href="{@docRoot}/../../../docs/zookeeperAdmin.html#sc_zkCommands">
-     * Zk Admin</a>. this link is for all the commands.
-     */
-    protected final static int ruokCmd =
-        ByteBuffer.wrap("ruok".getBytes()).getInt();
-    /*
-     * See <a href="{@docRoot}/../../../docs/zookeeperAdmin.html#sc_zkCommands">
-     * Zk Admin</a>. this link is for all the commands.
-     */
-    protected final static int setTraceMaskCmd =
-        ByteBuffer.wrap("stmk".getBytes()).getInt();
-
-    /*
-     * See <a href="{@docRoot}/../../../docs/zookeeperAdmin.html#sc_zkCommands">
-     * Zk Admin</a>. this link is for all the commands.
-     */
-    protected final static int srvrCmd =
-        ByteBuffer.wrap("srvr".getBytes()).getInt();
-
-    /*
-     * See <a href="{@docRoot}/../../../docs/zookeeperAdmin.html#sc_zkCommands">
-     * Zk Admin</a>. this link is for all the commands.
-     */
-    protected final static int srstCmd =
-        ByteBuffer.wrap("srst".getBytes()).getInt();
-
-    /*
-     * See <a href="{@docRoot}/../../../docs/zookeeperAdmin.html#sc_zkCommands">
-     * Zk Admin</a>. this link is for all the commands.
-     */
-    protected final static int statCmd =
-        ByteBuffer.wrap("stat".getBytes()).getInt();
-
-    /*
-     * See <a href="{@docRoot}/../../../docs/zookeeperAdmin.html#sc_zkCommands">
-     * Zk Admin</a>. this link is for all the commands.
-     */
-    protected final static int wchcCmd =
-        ByteBuffer.wrap("wchc".getBytes()).getInt();
-
-    /*
-     * See <a href="{@docRoot}/../../../docs/zookeeperAdmin.html#sc_zkCommands">
-     * Zk Admin</a>. this link is for all the commands.
-     */
-    protected final static int wchpCmd =
-        ByteBuffer.wrap("wchp".getBytes()).getInt();
-
-    /*
-     * See <a href="{@docRoot}/../../../docs/zookeeperAdmin.html#sc_zkCommands">
-     * Zk Admin</a>. this link is for all the commands.
-     */
-    protected final static int wchsCmd =
-        ByteBuffer.wrap("wchs".getBytes()).getInt();
-
-    /*
-     * See <a href="{@docRoot}/../../../docs/zookeeperAdmin.html#sc_zkCommands">
-     * Zk Admin</a>. this link is for all the commands.
-     */
-    protected final static int mntrCmd = ByteBuffer.wrap("mntr".getBytes())
-            .getInt();
-
-    /*
-     * See <a href="{@docRoot}/../../../docs/zookeeperAdmin.html#sc_zkCommands">
-     * Zk Admin</a>. this link is for all the commands.
-     */
-    protected final static int isroCmd = ByteBuffer.wrap("isro".getBytes())
-            .getInt();
-
-    /*
-     * The control sequence sent by the telnet program when it closes a
-     * connection. Include simply to keep the logs cleaner (the server would
-     * close the connection anyway because it would parse this as a negative
-     * length).
-     */
-    protected final static int telnetCloseCmd = 0xfff4fffd;
-
-    protected final static HashMap<Integer, String> cmd2String =
-        new HashMap<Integer, String>();
-
-    // specify all of the commands that are available
-    static {
-        cmd2String.put(confCmd, "conf");
-        cmd2String.put(consCmd, "cons");
-        cmd2String.put(crstCmd, "crst");
-        cmd2String.put(dumpCmd, "dump");
-        cmd2String.put(enviCmd, "envi");
-        cmd2String.put(getTraceMaskCmd, "gtmk");
-        cmd2String.put(ruokCmd, "ruok");
-        cmd2String.put(setTraceMaskCmd, "stmk");
-        cmd2String.put(srstCmd, "srst");
-        cmd2String.put(srvrCmd, "srvr");
-        cmd2String.put(statCmd, "stat");
-        cmd2String.put(wchcCmd, "wchc");
-        cmd2String.put(wchpCmd, "wchp");
-        cmd2String.put(wchsCmd, "wchs");
-        cmd2String.put(mntrCmd, "mntr");
-        cmd2String.put(isroCmd, "isro");
-        cmd2String.put(telnetCloseCmd, "telnet close");
     }
 
     protected void packetReceived() {
@@ -417,7 +276,7 @@ public abstract class ServerCnxn implements Stats, Watcher {
      * @param brief iff true prints brief details, otw full detail
      * @return information about this connection
      */
-    protected synchronized void
+    public synchronized void
     dumpConnectionInfo(PrintWriter pwriter, boolean brief) {
         pwriter.print(" ");
         pwriter.print(getRemoteSocketAddress());
@@ -485,5 +344,29 @@ public abstract class ServerCnxn implements Stats, Watcher {
             info.put("max_latency", getMaxLatency());
         }
         return info;
+    }
+
+    /**
+     * clean up the socket related to a command and also make sure we flush the
+     * data before we do that
+     *
+     * @param pwriter
+     *            the pwriter for a command socket
+     */
+    public void cleanupWriterSocket(PrintWriter pwriter) {
+        try {
+            if (pwriter != null) {
+                pwriter.flush();
+                pwriter.close();
+            }
+        } catch (Exception e) {
+            LOG.info("Error closing PrintWriter ", e);
+        } finally {
+            try {
+                close();
+            } catch (Exception e) {
+                LOG.error("Error closing a command socket ", e);
+            }
+        }
     }
 }
---
+++ src/java/main/org/apache/zookeeper/server/command/CommandExecutor.java
@@ -0,0 +1,79 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.zookeeper.server.command;
+
+import java.io.PrintWriter;
+
+import org.apache.zookeeper.server.ServerCnxn;
+import org.apache.zookeeper.server.ServerCnxnFactory;
+import org.apache.zookeeper.server.ZooKeeperServer;
+
+public class CommandExecutor {
+    /**
+     * This class decides which command to be executed and then executes
+     */
+    public boolean execute(ServerCnxn serverCnxn, PrintWriter pwriter,
+            final int commandCode, ZooKeeperServer zkServer, ServerCnxnFactory factory) {
+        AbstractFourLetterCommand command = getCommand(serverCnxn,pwriter, commandCode);
+
+        if (command == null) {
+            return false;
+        }
+
+        command.setZkServer(zkServer);
+        command.setFactory(factory);
+        command.start();
+        return true;
+    }
+
+    private AbstractFourLetterCommand getCommand(ServerCnxn serverCnxn,
+            PrintWriter pwriter, final int commandCode) {
+        AbstractFourLetterCommand command = null;
+        if (commandCode == FourLetterCommands.ruokCmd) {
+            command = new RuokCommand(pwriter, serverCnxn);
+        } else if (commandCode == FourLetterCommands.getTraceMaskCmd) {
+            command = new TraceMaskCommand(pwriter, serverCnxn);
+        } else if (commandCode == FourLetterCommands.enviCmd) {
+            command = new EnvCommand(pwriter, serverCnxn);
+        } else if (commandCode == FourLetterCommands.confCmd) {
+            command = new ConfCommand(pwriter, serverCnxn);
+        } else if (commandCode == FourLetterCommands.srstCmd) {
+            command = new StatResetCommand(pwriter, serverCnxn);
+        } else if (commandCode == FourLetterCommands.crstCmd) {
+            command = new CnxnStatResetCommand(pwriter, serverCnxn);
+        } else if (commandCode == FourLetterCommands.dumpCmd) {
+            command = new DumpCommand(pwriter, serverCnxn);
+        } else if (commandCode == FourLetterCommands.statCmd
+                || commandCode == FourLetterCommands.srvrCmd) {
+            command = new StatCommand(pwriter, serverCnxn, commandCode);
+        } else if (commandCode == FourLetterCommands.consCmd) {
+            command = new ConsCommand(pwriter, serverCnxn);
+        } else if (commandCode == FourLetterCommands.wchpCmd
+                || commandCode == FourLetterCommands.wchcCmd
+                || commandCode == FourLetterCommands.wchsCmd) {
+            command = new WatchCommand(pwriter, serverCnxn, commandCode);
+        } else if (commandCode == FourLetterCommands.mntrCmd) {
+            command = new MonitorCommand(pwriter, serverCnxn);
+        } else if (commandCode == FourLetterCommands.isroCmd) {
+            command = new IsroCommand(pwriter, serverCnxn);
+        }
+        return command;
+    }
+
+}
