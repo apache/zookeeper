f1f6eba885545e45ea345250ced342aa0d3383b1
Tang Xinye <tgttxy@163.com>
2015-06-30 11:41:28 +0800

    Update CGenerator.java

--- src/java/main/org/apache/jute/compiler/CGenerator.java
+++ src/java/main/org/apache/jute/compiler/CGenerator.java
@@ -61,10 +61,9 @@ class CGenerator {
                         + outputDirectory);
             }
         }
+        FileWriter c = new FileWriter(new File(outputDirectory, mName+".c"));
+        FileWriter h = new FileWriter(new File(outputDirectory, mName+".h"));
         try {
-        	FileWriter c = new FileWriter(new File(outputDirectory, mName+".c"));
-            FileWriter h = new FileWriter(new File(outputDirectory, mName+".h"));
-
             h.write("/**\n");
             h.write("* Licensed to the Apache Software Foundation (ASF) under one\n");
             h.write("* or more contributor license agreements.  See the NOTICE file\n");
@@ -121,11 +120,9 @@ class CGenerator {
                 JRecord jr = i.next();
                 jr.genCCode(h, c);
             }
-
             h.write("\n#ifdef __cplusplus\n}\n#endif\n\n");
             h.write("#endif //"+mName.toUpperCase().replace('.','_')+"__\n");
         } finally {
-
             h.close();
             c.close();
         }
