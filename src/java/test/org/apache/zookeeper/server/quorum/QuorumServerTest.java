08cc9187d9eb327b2a71bc9a8c62636815dfe9e5
Chris Nauroth <cnauroth@apache.org>
2015-10-18 17:00:41 +0000

    ZOOKEEPER-1460: IPv6 literal address not supported for quorum members (Joseph Walton via cnauroth)
    git-svn-id: https://svn.apache.org/repos/asf/zookeeper/trunk@1709293 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/test/org/apache/zookeeper/server/quorum/QuorumServerTest.java
+++ src/java/test/org/apache/zookeeper/server/quorum/QuorumServerTest.java
@@ -16,6 +16,8 @@
  */
 package org.apache.zookeeper.server.quorum;
 
+import static org.junit.Assert.assertEquals;
+
 import org.apache.zookeeper.ZKTestCase;
 import org.apache.zookeeper.server.quorum.QuorumPeer.QuorumServer;
 import org.apache.zookeeper.server.quorum.QuorumPeerConfig.ConfigException;
@@ -56,5 +58,29 @@ public class QuorumServerTest extends ZKTestCase {
         expected = "example.com:1234:1236:participant;0.0.0.0:1237";
         qs = new QuorumServer(0, config);
         Assert.assertEquals("Use hostname", expected, qs.toString());
+    }
+
+    @Test
+    public void constructionUnderstandsIpv6LiteralsInServerConfig() throws ConfigException {
+        String config = "[::1]:1234:1236:participant";
+        QuorumServer qs = new QuorumServer(0, config);
+        assertEquals("[0:0:0:0:0:0:0:1]:1234:1236:participant", qs.toString());
+    }
+
+    @Test
+    public void constructionUnderstandsIpv6LiteralsInClientConfig() throws ConfigException {
+        String config = "127.0.0.1:1234:1236:participant;[::1]:1237";
+        QuorumServer qs = new QuorumServer(0, config);
+        assertEquals("127.0.0.1:1234:1236:participant;[0:0:0:0:0:0:0:1]:1237", qs.toString());
+    }
+
+    @Test(expected = ConfigException.class)
+    public void unbalancedIpv6LiteralsInServerConfigFailToBeParsed() throws ConfigException {
+        new QuorumServer(0, "[::1:1234:1236:participant");
+    }
+
+    @Test(expected = ConfigException.class)
+    public void unbalancedIpv6LiteralsInClientConfigFailToBeParsed() throws ConfigException {
+        new QuorumServer(0, "127.0.0.1:1234:1236:participant;[::1:1237");
     }
 }
