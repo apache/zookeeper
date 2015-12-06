22b7c77cb49aa348f8deb578f06ab42bf418bbc9
Raúl Gutiérrez Segalés <rgs@apache.org>
2015-12-06 20:38:43 +0000

    ZOOKEEPER-2304: JMX ClientPort from ZooKeeperServerBean incorrect (Arshad Mohammad via rgs)
    git-svn-id: https://svn.apache.org/repos/asf/zookeeper/branches/branch-3.5@1718215 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/main/org/apache/zookeeper/server/ZooKeeperServerBean.java
+++ src/java/main/org/apache/zookeeper/server/ZooKeeperServerBean.java
@@ -18,8 +18,6 @@
 
 package org.apache.zookeeper.server;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.util.Date;
 
 import org.apache.zookeeper.Version;
@@ -41,12 +39,7 @@ public class ZooKeeperServerBean implements ZooKeeperServerMXBean, ZKMBeanInfo {
     }
     
     public String getClientPort() {
-        try {
-            return InetAddress.getLocalHost().getHostAddress() + ":"
-                + zks.getClientPort();
-        } catch (UnknownHostException e) {
-            return "localhost:" + zks.getClientPort();
-        }
+        return Integer.toString(zks.getClientPort());
     }
     
     public String getName() {
