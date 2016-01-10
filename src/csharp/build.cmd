5e8901034eb9663a8d6c987b5fdd8f1f8d7a4d42
Shay Hazor <shayhatsor@gmail.com>
2016-01-10 22:57:16 +0200

    align with dnx 1.0.0-rc1-update1

--- src/csharp/build.cmd
+++ src/csharp/build.cmd
@@ -2,39 +2,30 @@
 cd %~dp0
 
 SETLOCAL
-SET NUGET_VERSION=latest
-SET CACHED_NUGET=%LocalAppData%\NuGet\nuget.%NUGET_VERSION%.exe
-SET BUILDCMD_KOREBUILD_VERSION=
-SET BUILDCMD_DNX_VERSION=
+if exist .nuget RD /S /Q .nuget
+if exist packages RD /S /Q packages
+if exist artifacts RD /S /Q artifacts
 
-IF EXIST %CACHED_NUGET% goto copynuget
 echo Downloading latest version of NuGet.exe...
-IF NOT EXIST %LocalAppData%\NuGet md %LocalAppData%\NuGet
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest 'https://dist.nuget.org/win-x86-commandline/%NUGET_VERSION%/nuget.exe' -OutFile '%CACHED_NUGET%'"
-
-:copynuget
-IF EXIST .nuget\nuget.exe goto restore
 md .nuget
-copy %CACHED_NUGET% .nuget\nuget.exe > nul
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest 'https://dist.nuget.org/win-x86-commandline/latest/nuget.exe' -OutFile .nuget\NuGet.exe"
 
-:restore
-IF EXIST packages\Sake goto getdnx
-IF "%BUILDCMD_KOREBUILD_VERSION%"=="" (
-    .nuget\nuget.exe install KoreBuild -ExcludeVersion -o packages -nocache -pre
-) ELSE (
-    .nuget\nuget.exe install KoreBuild -version %BUILDCMD_KOREBUILD_VERSION% -ExcludeVersion -o packages -nocache -pre
-)
+echo Downloading KoreBuild...
+.nuget\nuget.exe install KoreBuild -ExcludeVersion -o packages -nocache -pre
+
+echo Downloading Sake...
 .nuget\NuGet.exe install Sake -ExcludeVersion -Source https://www.nuget.org/api/v2/ -Out packages
 
-:getdnx
-IF "%BUILDCMD_DNX_VERSION%"=="" (
-    SET BUILDCMD_DNX_VERSION=latest
-)
-IF "%SKIP_DNX_INSTALL%"=="" (
-    CALL packages\KoreBuild\build\dnvm install %BUILDCMD_DNX_VERSION% -runtime CoreCLR -arch x86 -alias default
-    CALL packages\KoreBuild\build\dnvm install default -runtime CLR -arch x86 -alias default
-) ELSE (
-    CALL packages\KoreBuild\build\dnvm use default -runtime CLR -arch x86
-)
+echo Updating DNVM...
+CALL packages\KoreBuild\build\dnvm update-self
 
+echo Downloading DNX...
+CALL packages\KoreBuild\build\dnvm install latest -runtime CoreCLR -arch x86 -alias default
+CALL packages\KoreBuild\build\dnvm install default -runtime CLR -arch x86 -alias default
+
+echo Running Sake Build...
 packages\Sake\tools\Sake.exe -I packages\KoreBuild\build -f makefile.shade %*
+
+echo Packing nugets...
+call dnu pack src\ZooKeeperNetEx --configuration release --out artifacts\nugets
+call dnu pack src\ZooKeeperNetEx.Recipes --configuration release --out artifacts\nugets
