c434332661aaff7cfb2d959457b18ba7ee4ddb2a
Thomas <thomaslau@FuckignMacbook.local>
2016-01-11 17:59:48 +0800

    fix typo of java docs in OutputArchive.java

--- src/java/main/org/apache/jute/OutputArchive.java
+++ src/java/main/org/apache/jute/OutputArchive.java
@@ -23,7 +23,7 @@ import java.util.List;
 import java.util.TreeMap;
 
 /**
- * Interface that alll the serializers have to implement.
+ * Interface that all the serializers have to implement.
  *
  */
 public interface OutputArchive {
