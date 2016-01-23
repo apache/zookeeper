06d3f3fa1bff258e62c0670309ad1849b1434bb1
Rakesh Radhakrishnan <rakeshr@apache.org>
2016-01-23 05:06:58 +0000

    Revert ZOOKEEPER-2297
    git-svn-id: https://svn.apache.org/repos/asf/zookeeper/trunk@1726354 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/test/org/apache/zookeeper/test/SSLTest.java
+++ src/java/test/org/apache/zookeeper/test/SSLTest.java
@@ -50,7 +50,6 @@ public class SSLTest extends QuorumPeerTestBase {
         System.setProperty(X509Util.SSL_KEYSTORE_PASSWD, "testpass");
         System.setProperty(X509Util.SSL_TRUSTSTORE_LOCATION, testDataPath + "/ssl/testTrustStore.jks");
         System.setProperty(X509Util.SSL_TRUSTSTORE_PASSWD, "testpass");
-        System.setProperty("zookeeper.authProvider.x509", "org.apache.zookeeper.server.auth.X509AuthenticationProvider");
     }
 
     @After
