8616a9ec8ce4bc8ac2987b7417a6f0c4b7333658
Rakesh Radhakrishnan <rakeshr@apache.org>
2016-12-20 13:47:25 +0000

    ZOOKEEPER-2479: Add 'electionTimeTaken' value in LeaderMXBean and FollowerMXBean
    Author: Rakesh Radhakrishnan <rakeshr@apache.org>
    
    Reviewers: Edward Ribeiro <edward.ribeiro@gmail.com>, fpj <fpj@apache.org>
    
    Closes #98 from rakeshadr/ZK-2479 and squashes the following commits:
    
    2c9eadf [Rakesh Radhakrishnan] ZOOKEEPER-2479: avoids duplicate code
    5af1314 [Rakesh Radhakrishnan] ZOOKEEPER-2479: Fixed Raul's review comments
    cc011a0 [Rakesh Radhakrishnan] ZOOKEEPER-2479: Add 'electionTimeTaken' value in LeaderMXBean and FollowerMXBean

--- src/java/test/org/apache/zookeeper/test/QuorumMajorityTest.java
+++ src/java/test/org/apache/zookeeper/test/QuorumMajorityTest.java
@@ -17,14 +17,16 @@
  */
 
 package org.apache.zookeeper.test;
-import java.util.HashSet;
+import java.util.ArrayList;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.apache.zookeeper.server.quorum.Leader;
+import org.apache.zookeeper.jmx.CommonNames;
 import org.apache.zookeeper.server.quorum.Leader.Proposal;
+import org.apache.zookeeper.server.quorum.QuorumPeer;
+import org.apache.zookeeper.server.quorum.QuorumPeer.ServerState;
 import org.junit.Assert;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class QuorumMajorityTest extends QuorumBase {
     protected static final Logger LOG = LoggerFactory.getLogger(QuorumMajorityTest.class);
@@ -36,7 +38,28 @@ public class QuorumMajorityTest extends QuorumBase {
     /***************************************************************/
     @Test
     public void testMajQuorums() throws Throwable {
-       
+        LOG.info("Verify QuorumPeer#electionTimeTaken jmx bean attribute");
+
+        ArrayList<QuorumPeer> peers = getPeerList();
+        for (int i = 1; i <= peers.size(); i++) {
+            QuorumPeer qp = peers.get(i - 1);
+            Long electionTimeTaken = -1L;
+            String bean = "";
+            if (qp.getPeerState() == ServerState.FOLLOWING) {
+                bean = String.format(
+                        "%s:name0=ReplicatedServer_id%d,name1=replica.%d,name2=Follower",
+                        CommonNames.DOMAIN, i, i);
+            } else if (qp.getPeerState() == ServerState.LEADING) {
+                bean = String.format(
+                        "%s:name0=ReplicatedServer_id%d,name1=replica.%d,name2=Leader",
+                        CommonNames.DOMAIN, i, i);
+            }
+            electionTimeTaken = (Long) JMXEnv.ensureBeanAttribute(bean,
+                    "ElectionTimeTaken");
+            Assert.assertTrue("Wrong electionTimeTaken value!",
+                    electionTimeTaken >= 0);
+        }
+
        //setup servers 1-5 to be followers
        setUp(false);
         
