01459c85b35178f2fbcc59a817da48457e678d39
Mohammad Arshad <arshad@apache.org>
2017-01-31 01:40:20 +0530

    ZOOKEEPER-2680:Correct DataNode.getChildren() inconsistent behaviour.

--- src/java/main/org/apache/zookeeper/server/DataNode.java
+++ src/java/main/org/apache/zookeeper/server/DataNode.java
@@ -57,6 +57,8 @@ public class DataNode implements Record {
      */
     private Set<String> children = null;
 
+    private static final Set<String> EMPTY_SET = Collections.emptySet();
+
     /**
      * default constructor for the datanode
      */
@@ -126,7 +128,7 @@ public class DataNode implements Record {
      */
     public synchronized Set<String> getChildren() {
         if (children == null) {
-            return children;
+            return EMPTY_SET;
         }
 
         return Collections.unmodifiableSet(children);
