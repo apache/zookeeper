c98d44a7af8e7883eb7e37d165b3ececb1f5cd7d
Jiafu Jiang <jiangjiafu1989@gmail.com>
2017-11-15 13:33:52 -0800

    ZOOKEEPER-2923: The comment of the variable matchSyncs in class CommitProcessor has a mistake.
    Author: Jiafu Jiang <jiangjiafu1989@gmail.com>
    
    Reviewers: phunt@apache.org
    
    Closes #408 from JiangJiafu/ZOOKEEPER-2923
    
    Change-Id: Iaf5076b38aa950467b2664684ba98743ab131a5b
    (cherry picked from commit 8be14c4d4743bebfd3e2b1759503d640480d4218)
    Signed-off-by: Patrick Hunt <phunt@apache.org>

--- src/java/main/org/apache/zookeeper/server/quorum/CommitProcessor.java
+++ src/java/main/org/apache/zookeeper/server/quorum/CommitProcessor.java
@@ -106,7 +106,7 @@ public class CommitProcessor extends ZooKeeperCriticalThread implements
     /**
      * This flag indicates whether we need to wait for a response to come back from the
      * leader or we just let the sync operation flow through like a read. The flag will
-     * be true if the CommitProcessor is in a Leader pipeline.
+     * be false if the CommitProcessor is in a Leader pipeline.
      */
     boolean matchSyncs;
 
