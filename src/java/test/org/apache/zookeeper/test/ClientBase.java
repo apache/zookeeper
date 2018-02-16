9b969d450c17c9c1790510333f688562c064aac7
Mark Fenes <mfenes@cloudera.com>
2018-02-16 23:28:07 +0100

    ZOOKEEPER-2967: Add check to validate dataDir and dataLogDir parameters at startup
    Added the todo comment back to be in sync with master.

--- src/java/test/org/apache/zookeeper/test/ClientBase.java
+++ src/java/test/org/apache/zookeeper/test/ClientBase.java
@@ -383,7 +383,8 @@ public abstract class ClientBase extends ZKTestCase {
         File tmpDir = new File(tmpFile + ".dir");
         Assert.assertFalse(tmpDir.exists()); // never true if tmpfile does it's job
         Assert.assertTrue(tmpDir.mkdirs());
-        
+
+        // todo not every tmp directory needs this file
         if (createInitFile) {
             createInitializeFile(tmpDir);
         }
