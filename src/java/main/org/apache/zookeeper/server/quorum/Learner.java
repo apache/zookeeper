4f8f3ce8074b878f2a6e32c15ec177f4dcd050e0
Eron Wright <eron.wright@gmail.com>
2018-02-19 15:05:44 -0800

    ZOOKEEPER-2982 Re-try DNS hostname -> IP resolution if node connection fails

--- src/java/main/org/apache/zookeeper/server/quorum/Learner.java
+++ src/java/main/org/apache/zookeeper/server/quorum/Learner.java
@@ -201,6 +201,9 @@ public class Learner {
         Vote current = self.getCurrentVote();
         for (QuorumServer s : self.getView().values()) {
             if (s.id == current.getId()) {
+                // Ensure we have the leader's correct IP address before
+                // attempting to connect.
+                s.recreateSocketAddresses();
                 leaderServer = s;
                 break;
             }
