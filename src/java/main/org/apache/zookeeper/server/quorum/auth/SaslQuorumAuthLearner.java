751763553f83e66874e695b0111559e7e7608e03
gongleigl.gong <gongleigl.gong@qunar.com>
2018-03-28 19:25:19 +0800

    del catch

--- src/java/main/org/apache/zookeeper/server/quorum/auth/SaslQuorumAuthLearner.java
+++ src/java/main/org/apache/zookeeper/server/quorum/auth/SaslQuorumAuthLearner.java
@@ -138,8 +138,6 @@ public class SaslQuorumAuthLearner implements QuorumAuthLearner {
 
             // Validate status code at the end of authentication exchange.
             checkAuthStatus(sock, qpStatus);
-        } catch (RuntimeException e) {
-            // do nothing
         } finally {
             if (sc != null) {
                 try {
