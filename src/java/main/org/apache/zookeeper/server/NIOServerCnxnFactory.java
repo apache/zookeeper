674e4403ebbb9c368baada264fb4eaff9898f984
LJ1043041006 <1239497420@qq.com>
2018-05-21 14:28:56 +0800

    add IOException

--- src/java/main/org/apache/zookeeper/server/NIOServerCnxnFactory.java
+++ src/java/main/org/apache/zookeeper/server/NIOServerCnxnFactory.java
@@ -815,9 +815,12 @@ public class NIOServerCnxnFactory extends ServerCnxnFactory {
         cnxnExpiryQueue.update(cnxn, cnxn.getSessionTimeout());
     }
 
-    private void addCnxn(NIOServerCnxn cnxn) {
+    private void addCnxn(NIOServerCnxn cnxn) throws IOException {
         InetAddress addr = cnxn.getSocketAddress();
-        Set<NIOServerCnxn> set = (addr == null ? null : ipMap.get(addr));
+        if (addr == null) {
+            throw new IOException("Scoket of " + cnxn + " has been closed");
+        }
+        Set<NIOServerCnxn> set = ipMap.get(addr);
         if (set == null) {
             // in general we will see 1 connection from each
             // host, setting the initial cap to 2 allows us
