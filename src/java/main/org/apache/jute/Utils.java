89239f84b1d322f170973ccdafa989508a6b8f09
Hosur Narahari <hosur.narhari@impetus.co.in>
2018-06-08 22:02:00 +0530

    improving compareBytes method of Utils class

--- src/java/main/org/apache/jute/Utils.java
+++ src/java/main/org/apache/jute/Utils.java
@@ -268,14 +268,13 @@ public class Utils {
         return stream.toByteArray();
     }
     public static int compareBytes(byte b1[], int off1, int len1, byte b2[], int off2, int len2) {
-        int i;
-        for(i=0; i < len1 && i < len2; i++) {
+        if (len1 != len2) {
+            return len1 < len2 ? -1 : 1;
+        }
+        for(int i=0; i < len1; i++) {
             if (b1[off1+i] != b2[off2+i]) {
                 return b1[off1+i] < b2[off2+i] ? -1 : 1;
             }
-        }
-        if (len1 != len2) {
-            return len1 < len2 ? -1 : 1;
         }
         return 0;
     }
