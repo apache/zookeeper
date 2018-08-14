82776922af9a0931e8053c0b93b82311e0f0b4b3
Enrico Olivelli - Diennea <eolivelli@apache.org>
2018-08-14 14:33:45 +0200

    ZOOKEEPER-3103: Pluggable metrics system for ZooKeeper - MetricsProvider API definition
    Define the API which must be implemented by a Metrics Provider.
    
    a MetricsProvider is a pluggable component which is able to gather metrics about the system and publish them to an external application for analysis.
    
    Author: Enrico Olivelli - Diennea <eolivelli@apache.org>
    Author: Enrico Olivelli <eolivelli@apache.org>
    
    Reviewers: fangmin@apache.org, andor@apache.org
    
    Closes #582 from eolivelli/fix/metrics and squashes the following commits:
    
    16412653 [Enrico Olivelli] better javadocs
    c3fc95e9 [Enrico Olivelli - Diennea] Address review comments, clean up API and docs, add 'boolean' return value to registerGauge
    f57e2f33 [Enrico Olivelli - Diennea] Address review comments, clean up API and docs, add 'boolean' return value to registerGauge
    08ca334f [Enrico Olivelli - Diennea] Address review comments, clean up API and docs, add 'boolean' return value to registerGauge
    0640293b [Enrico Olivelli - Diennea] ZOOKEEPER-3103 Pluggable metrics system for ZooKeeper - MetricsProvider API definition

---
+++ src/java/main/org/apache/zookeeper/metrics/MetricsProviderLifeCycleException.java
@@ -0,0 +1,45 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.zookeeper.metrics;
+
+/**
+ * A generic exception thrown during the licecycle of a MetricsProvider.
+ * <p>These exception will prevent the system from booting.</p>
+ * <p>Normally these exception will be ignored during shutdown.</p>
+ */
+public class MetricsProviderLifeCycleException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    public MetricsProviderLifeCycleException() {
+    }
+
+    public MetricsProviderLifeCycleException(String message) {
+        super(message);
+    }
+
+    public MetricsProviderLifeCycleException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public MetricsProviderLifeCycleException(Throwable cause) {
+        super(cause);
+    }
+
+}
