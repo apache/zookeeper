7b503ff590c971c64185f2ccdd138f8aee9c5381
Norbert Kalmar <nkalmar@yahoo.com>
2018-08-21 11:32:45 +0200

    ZOOKEEPER-3031 3.4: MAVEN MIGRATION - move client dir

--- zookeeper-contrib/zookeeper-contrib-zkpython/src/python/setup.py
+++ zookeeper-contrib/zookeeper-contrib-zkpython/src/python/setup.py
@@ -19,12 +19,12 @@ from distutils.core import setup, Extension
 zookeeper_basedir = "../../../"
 
 zookeepermodule = Extension("zookeeper",
-                            sources=["src/c/zookeeper.c"],
-                            include_dirs=[zookeeper_basedir + "/src/c/include",
+                            sources=["zookeeper-client/zookeeper-client-c/zookeeper.c"],
+                            include_dirs=[zookeeper_basedir + "/zookeeper-client/zookeeper-client-c/include",
                                           zookeeper_basedir + "/build/c",
-                                          zookeeper_basedir + "/src/c/generated"],
+                                          zookeeper_basedir + "/zookeeper-client/zookeeper-client-c/generated"],
                             libraries=["zookeeper_mt"],
-                            library_dirs=[zookeeper_basedir + "/src/c/.libs/",
+                            library_dirs=[zookeeper_basedir + "/zookeeper-client/zookeeper-client-c/.libs/",
                                           zookeeper_basedir + "/build/c/.libs/",
                                           zookeeper_basedir + "/build/test/test-cppunit/.libs",
                                           "/usr/local/lib"
