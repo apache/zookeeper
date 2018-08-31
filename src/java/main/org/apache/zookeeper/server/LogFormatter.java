298428eaf7f6c2f811e01868507f2e1e77ec3bc7
maoling <maoling199210191@sina.com>
2018-08-31 19:12:05 +0800

    ZOOKEEPER-1823:LogFormatter should support printing transaction data as a string

--- src/java/main/org/apache/zookeeper/server/LogFormatter.java
+++ src/java/main/org/apache/zookeeper/server/LogFormatter.java
@@ -26,6 +26,9 @@ import java.util.Date;
 import java.util.zip.Adler32;
 import java.util.zip.Checksum;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.PosixParser;
 import org.apache.jute.BinaryInputArchive;
 import org.apache.jute.Record;
 import org.apache.yetus.audience.InterfaceAudience;
@@ -34,6 +37,10 @@ import org.slf4j.LoggerFactory;
 import org.apache.zookeeper.server.persistence.FileHeader;
 import org.apache.zookeeper.server.persistence.FileTxnLog;
 import org.apache.zookeeper.server.util.SerializeUtils;
+import org.apache.zookeeper.txn.CreateContainerTxn;
+import org.apache.zookeeper.txn.CreateTTLTxn;
+import org.apache.zookeeper.txn.CreateTxn;
+import org.apache.zookeeper.txn.SetDataTxn;
 import org.apache.zookeeper.txn.TxnHeader;
 
 @InterfaceAudience.Public
@@ -41,25 +48,69 @@ public class LogFormatter {
     private static final Logger LOG = LoggerFactory.getLogger(LogFormatter.class);
 
     /**
+     * get transaction log data string with node's data as a string
+     * @param txn
+     * @return
+     */
+    private static String getDataStrFromTxn(Record txn) {
+        StringBuilder txnData = new StringBuilder();
+        if (txn == null) {
+            return txnData.toString();
+        }
+        if (txn instanceof CreateTxn) {
+            CreateTxn createTxn = ((CreateTxn) txn);
+            txnData.append(createTxn.getPath() + "," + new String(createTxn.getData()))
+                   .append("," + createTxn.getAcl() + "," + createTxn.getEphemeral())
+                   .append("," + createTxn.getParentCVersion());
+        } else if (txn instanceof SetDataTxn) {
+            SetDataTxn setDataTxn = ((SetDataTxn) txn);
+            txnData.append(setDataTxn.getPath() + "," + new String(setDataTxn.getData()))
+                   .append("," + setDataTxn.getVersion());
+        } else if (txn instanceof CreateContainerTxn) {
+            CreateContainerTxn createContainerTxn = ((CreateContainerTxn) txn);
+            txnData.append(createContainerTxn.getPath() + "," + new String(createContainerTxn.getData()))
+                   .append("," + createContainerTxn.getAcl() + "," + createContainerTxn.getParentCVersion());
+        } else if (txn instanceof CreateTTLTxn) {
+            CreateTTLTxn createTTLTxn = ((CreateTTLTxn) txn);
+            txnData.append(createTTLTxn.getPath() + "," + new String(createTTLTxn.getData()))
+                   .append("," + createTTLTxn.getAcl() + "," + createTTLTxn.getParentCVersion())
+                   .append("," + createTTLTxn.getTtl());
+        } else {
+            txnData.append(txn.toString());
+        }
+
+        return txnData.toString();
+    }
+    /**
      * @param args
      */
     public static void main(String[] args) throws Exception {
-        if (args.length != 1) {
+        CommandLine cl;
+        Options options = new Options();
+        PosixParser parser = new PosixParser();
+        String[] cmdArgs;
+
+        options.addOption("s", false, "showdata");
+        cl = parser.parse(options, args);
+        cmdArgs = cl.getArgs();
+
+        if (cmdArgs.length != 1) {
             System.err.println("USAGE: LogFormatter log_file");
+            System.err.println("USAGE: LogFormatter [-s] log_file to print transaction data as a string");
             System.exit(ExitCode.INVALID_INVOCATION.getValue());
         }
-        FileInputStream fis = new FileInputStream(args[0]);
+        FileInputStream fis = new FileInputStream(cmdArgs[0]);
         BinaryInputArchive logStream = BinaryInputArchive.getArchive(fis);
         FileHeader fhdr = new FileHeader();
         fhdr.deserialize(logStream, "fileheader");
 
         if (fhdr.getMagic() != FileTxnLog.TXNLOG_MAGIC) {
-            System.err.println("Invalid magic number for " + args[0]);
+            System.err.println(String.format("Invalid magic number for %s", cmdArgs[0]));
             System.exit(ExitCode.INVALID_INVOCATION.getValue());
         }
-        System.out.println("ZooKeeper Transactional Log File with dbid "
-                + fhdr.getDbid() + " txnlog format version "
-                + fhdr.getVersion());
+        System.out.println(String.format("%s with dbid %d txnlog format version %d\n",
+                "ZooKeeper Transactional Log File", fhdr.getDbid(), fhdr.getVersion()));
+        DateFormat dataFormat = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.LONG);
 
         int count = 0;
         while (true) {
@@ -70,32 +121,33 @@ public class LogFormatter {
 
                 bytes = logStream.readBuffer("txnEntry");
             } catch (EOFException e) {
-                System.out.println("EOF reached after " + count + " txns.");
+                System.out.println(String.format("EOF reached after %d txns.", count));
                 return;
             }
             if (bytes.length == 0) {
                 // Since we preallocate, we define EOF to be an
                 // empty transaction
-                System.out.println("EOF reached after " + count + " txns.");
+                System.out.println(String.format("EOF reached after %d txns.", count));
                 return;
             }
             Checksum crc = new Adler32();
             crc.update(bytes, 0, bytes.length);
             if (crcValue != crc.getValue()) {
-                throw new IOException("CRC doesn't match " + crcValue +
-                        " vs " + crc.getValue());
+                throw new IOException(String.format("CRC doesn't match %s vs %s", crcValue, crc.getValue()));
             }
             TxnHeader hdr = new TxnHeader();
             Record txn = SerializeUtils.deserializeTxn(bytes, hdr);
-            System.out.println(DateFormat.getDateTimeInstance(DateFormat.SHORT,
-                    DateFormat.LONG).format(new Date(hdr.getTime()))
-                    + " session 0x"
-                    + Long.toHexString(hdr.getClientId())
-                    + " cxid 0x"
-                    + Long.toHexString(hdr.getCxid())
-                    + " zxid 0x"
-                    + Long.toHexString(hdr.getZxid())
-                    + " " + TraceFormatter.op2String(hdr.getType()) + " " + txn);
+
+            String txnStr = (txn != null) ? txn.toString() : "";
+            if (cl.hasOption("s")) {
+                txnStr = getDataStrFromTxn(txn);
+            }
+
+            System.out.println(String.format("%s session 0x%s cxid 0x%s zxid 0x%s %s %s\n",
+                    dataFormat.format(new Date(hdr.getTime())), Long.toHexString(hdr.getClientId()),
+                    Long.toHexString(hdr.getCxid()), Long.toHexString(hdr.getZxid()),
+                    TraceFormatter.op2String(hdr.getType()), txnStr));
+
             if (logStream.readByte("EOR") != 'B') {
                 LOG.error("Last transaction was partial.");
                 throw new EOFException("Last transaction was partial.");
