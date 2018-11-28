e9e8a40d22c5b59eb285630e1a28fa7b3825948e
Karthik K <karthiknetworklink@gmail.com>
2018-11-28 12:11:35 +0530

    ZOOKEEPER-3205: remove unnecessary import

--- zookeeper-jute/src/test/java/org/apache/jute/BinaryInputArchiveTest.java
+++ zookeeper-jute/src/test/java/org/apache/jute/BinaryInputArchiveTest.java
@@ -24,7 +24,6 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 
 // TODO: introduce JuteTestCase as in ZKTestCase
