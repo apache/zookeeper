17ea64c28e9642db7a7cbc797c040ae59d350d27
Karthik K <karthiknetworklink@gmail.com>
2018-11-28 13:55:59 +0530

    ZOOKEEPER-3205: reorder static first

--- zookeeper-jute/src/test/java/org/apache/jute/BinaryInputArchiveTest.java
+++ zookeeper-jute/src/test/java/org/apache/jute/BinaryInputArchiveTest.java
@@ -17,11 +17,12 @@
  */
 package org.apache.jute;
 
-import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
