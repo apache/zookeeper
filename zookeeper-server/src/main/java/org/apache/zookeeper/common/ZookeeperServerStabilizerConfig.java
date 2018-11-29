05a512405ac8c1d93473a7ee10b4622f6905b695
vtumati <vtumati@paypal.com>
2018-11-29 06:51:24 +0530

    formatting

--- zookeeper-server/src/main/java/org/apache/zookeeper/common/ZookeeperServerStabilizerConfig.java
+++ zookeeper-server/src/main/java/org/apache/zookeeper/common/ZookeeperServerStabilizerConfig.java
@@ -22,23 +22,23 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ZookeeperServerStabilizerConfig {
-	protected static final Logger LOG = LoggerFactory.getLogger(ZookeeperServerStabilizerConfig.class);
+    protected static final Logger LOG = LoggerFactory.getLogger(ZookeeperServerStabilizerConfig.class);
 
-	public static final String GLOBAL_OUTSTANDING_LIMIT = "zookeeper.globalOutstandingLimit";
+    public static final String GLOBAL_OUTSTANDING_LIMIT = "zookeeper.globalOutstandingLimit";
 
-	private int globalOutstandingLimit = 1000;
+    private int globalOutstandingLimit = 1000;
 
-	public ZookeeperServerStabilizerConfig() {
-		setGlobalOutstandingLimit(Integer.getInteger(GLOBAL_OUTSTANDING_LIMIT, 1000));
-		LOG.info("{} = {}", GLOBAL_OUTSTANDING_LIMIT, getGlobalOutstandingLimit());
-	}
+    public ZookeeperServerStabilizerConfig() {
+	setGlobalOutstandingLimit(Integer.getInteger(GLOBAL_OUTSTANDING_LIMIT, 1000));
+	LOG.info("{} = {}", GLOBAL_OUTSTANDING_LIMIT, getGlobalOutstandingLimit());
+    }
 
-	public int getGlobalOutstandingLimit() {
-		return globalOutstandingLimit;
-	}
+    public int getGlobalOutstandingLimit() {
+	return globalOutstandingLimit;
+    }
 
-	public void setGlobalOutstandingLimit(int globalOutstandingLimit) {
-		this.globalOutstandingLimit = globalOutstandingLimit;
-	}
+    public void setGlobalOutstandingLimit(int globalOutstandingLimit) {
+	this.globalOutstandingLimit = globalOutstandingLimit;
+    }
 
 }
