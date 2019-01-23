f7d34ad021a6f6acb8c0d828047877223ad6f65d
Samuel Just <sjust@salesforce.com>
2019-01-23 14:51:13 -0800

    ZOOKEEPER-3253: client should not send requests with cxid=-4, -2, or -1
    - Add test for cxid rollover to 1
    - Modify ClientCnxn.SendThread.getXid() to increment from MAX to 1.
    
    Author: Samuel Just <sjust@salesforce.com>
    
    Reviewers: phunt@apache.org
    
    Closes #787 from athanatos/forupstream/ZOOKEEPER-3253
    
    Change-Id: Ib3d111170bb086d6982f2cf0ee5cf8afd5157588
    (cherry picked from commit e10c93a590cc1b73eebad48d18cfcbceb3ec0d4d)
    
    Includes backport of createConnection testability refactor
    from 9f82798415351a20136ceb1640b1781723e51cc1.
    
    Signed-off-by: Samuel Just <sjust@salesforce.com>

--- zookeeper-server/src/main/java/org/apache/zookeeper/ClientCnxn.java
+++ zookeeper-server/src/main/java/org/apache/zookeeper/ClientCnxn.java
@@ -1383,7 +1383,8 @@ public class ClientCnxn {
         }
     }
 
-    private int xid = 1;
+    // @VisibleForTesting
+    protected int xid = 1;
 
     // @VisibleForTesting
     volatile States state = States.NOT_CONNECTED;
@@ -1393,6 +1394,12 @@ public class ClientCnxn {
      * the server. Thus, getXid() must be public.
      */
     synchronized public int getXid() {
+        // Avoid negative cxid values.  In particular, cxid values of -4, -2, and -1 are special and
+        // must not be used for requests -- see SendThread.readResponse.
+        // Skip from MAX to 1.
+        if (xid == Integer.MAX_VALUE) {
+            xid = 1;
+        }
         return xid++;
     }
 
