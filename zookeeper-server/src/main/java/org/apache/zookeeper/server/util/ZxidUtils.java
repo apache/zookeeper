aeb77518ca265ed87e0bf9a73facdcefc2fa0154
asdf2014 <asdf2014@apache.org>
2019-03-03 19:59:31 +0800

    ZOOKEEPER-2789: Reassign `ZXID` for solving 32bit overflow problem(base master and fix conflict)

--- zookeeper-server/src/main/java/org/apache/zookeeper/server/util/ZxidUtils.java
+++ zookeeper-server/src/main/java/org/apache/zookeeper/server/util/ZxidUtils.java
@@ -19,18 +19,40 @@
 package org.apache.zookeeper.server.util;
 
 public class ZxidUtils {
+    private static final long EPOCH_HIGH_POSITION = 40L;
+    private static final long COUNTER_LOW_POSITION = 0xffffffffffL;
+    private static final long CLEAR_EPOCH = 0x000000ffffffffffL;
+    private static final long CLEAR_COUNTER = 0xffffff0000000000L;
 
-    public static long getEpochFromZxid(long zxid) {
-        return zxid >> 32L;
+    static public long getEpochFromZxid(long zxid) {
+        return zxid >> EPOCH_HIGH_POSITION;
     }
-    public static long getCounterFromZxid(long zxid) {
-        return zxid & 0xffffffffL;
+
+    static public long getCounterFromZxid(long zxid) {
+        return zxid & COUNTER_LOW_POSITION;
     }
-    public static long makeZxid(long epoch, long counter) {
-        return (epoch << 32L) | (counter & 0xffffffffL);
+
+    static public long makeZxid(long epoch, long counter) {
+        return (epoch << EPOCH_HIGH_POSITION) | (counter & COUNTER_LOW_POSITION);
     }
-    public static String zxidToString(long zxid) {
+
+    static public long clearEpoch(long zxid) {
+        return zxid & CLEAR_EPOCH;
+    }
+
+    static public long clearCounter(long zxid) {
+        return zxid & CLEAR_COUNTER;
+    }
+
+    static public String zxidToString(long zxid) {
         return Long.toHexString(zxid);
     }
 
+    public static long getEpochHighPosition() {
+        return EPOCH_HIGH_POSITION;
+    }
+
+    public static long getCounterLowPosition() {
+        return COUNTER_LOW_POSITION;
+    }
 }
