12799117a3bc69eaff6366f86f2b923b3b957df7
Stephen Goeddel <sgoeddel247@gmail.com>
2019-03-05 19:54:55 -0500

    Revert "testing new type of cache"
    This reverts commit 9d68a8f0

--- zookeeper-server/src/main/java/org/apache/zookeeper/server/DataCache.java
+++ zookeeper-server/src/main/java/org/apache/zookeeper/server/DataCache.java
@@ -10,78 +10,83 @@
 
 package org.apache.zookeeper.server;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
+import java.util.*;
 
 public class DataCache {
-    private static final Logger LOG = LoggerFactory.getLogger(DataCache.class);
-
     private static final long ONE_MB = 1024 * 1024;
     private static final int MAX_SIZE_IN_MB = 1;
 
-    private LinkedHashMap<String, DataCacheRecord> cachedNodes = new LinkedHashMap<>(); // Kept in order by LRU first
+    private Map<String, Long> allNodes = new HashMap<>();
+    private LinkedHashMap<String, Long> cachedNodes = new LinkedHashMap<>(); // Kept in order by LRU first
+    private List<String> nodesPendingEviction = Collections.synchronizedList(new ArrayList<>());
     private int currentSize;
 
 
-    public synchronized boolean put(String path, DataNode node, long size) {
-        LOG.info("Adding " + path + " to cache...");
+    /**
+     *
+     * @param pathToNode
+     * @return whether the node was already in the cache
+     */
+    public synchronized boolean markNodeAsAccessed(String pathToNode) {
         boolean nodeInCache = false;
-        if (cachedNodes.keySet().contains(path)) {
-            cachedNodes.remove(path); // Remove as it will need to be appended to the end of the LinkedHashMap
+        if (cachedNodes.keySet().contains(pathToNode)) {
+            cachedNodes.remove(pathToNode); // Remove as it will need to be appended to the end of the LinkedHashMap
             nodeInCache = true;
-            LOG.info("Node already in cache...");
         }
-        trim(size);
-        cachedNodes.put(path, new DataCacheRecord(path, node, size));
+
+        long size = allNodes.get(pathToNode);
+        if (size <= 0) {
+            throw new IllegalArgumentException("Attempted to mark a node as accessed when it was not found in the list of all tracked nodes.");
+        }
+
+        removeLeastRecentlyUsedNodePathsUntilCanAccommodateSize(size);
+
+        cachedNodes.put(pathToNode, size);
         currentSize += size;
-        LOG.info("Success. New cache size: " + Integer.toString(currentSize));
+
         return nodeInCache;
     }
 
-    public DataNode remove(String path) {
-        LOG.info("Removing " + path + " to cache...");
-        DataCacheRecord record = cachedNodes.remove(path);
-        if (record != null) {
-            currentSize -= record.size;
-            LOG.info("Success. New cache size: " + Integer.toString(currentSize));
-            return record.node;
-        } else {
-            LOG.info("Node not found.");
-            return null;
-        }
+    public void addNodeToAllNodes(String path, long size) {
+        allNodes.put(path, size);
     }
 
-    public void update(String path, DataNode node, long size) {
-        LOG.info("Updating " + path + " in cache...");
+    public void removeNode(String path) {
+        allNodes.remove(path);
         if (cachedNodes.containsKey(path)) {
-            DataCacheRecord oldRecord = cachedNodes.remove(path);
-            if (oldRecord != null) {
-                cachedNodes.put(path, new DataCacheRecord(path, node, size));
-                long sizeDifference = size - oldRecord.size;
-                currentSize += sizeDifference;
-                // Just in case we have gone over our cache size
-                trim(0L);
-                LOG.info("Success.");
-            } else {
-                LOG.info("Node not found.");
-            }
+            long sizeOfRemovedNode = cachedNodes.remove(path);
+            currentSize -= sizeOfRemovedNode;
         }
     }
 
-    public ArrayList<String> toArrayList() {
-        return new ArrayList<>(cachedNodes.keySet());
+    public void updateSizeOfNode(String path, long size) {
+        allNodes.put(path, size);
+
+        if (cachedNodes.containsKey(path)) {
+            long oldSize = cachedNodes.remove(path);
+            cachedNodes.put(path, size);
+            long sizeDifference = size - oldSize;
+            currentSize += sizeDifference;
+
+            // Just in case we have gone over our cache size
+            removeLeastRecentlyUsedNodePathsUntilCanAccommodateSize(0L);
+        }
     }
 
-    private void trim(long size) {
+    public List<String> getAndClearNodesPendingEviction() {
+        List<String> nodesPendingEvictionCopy = new ArrayList<>(nodesPendingEviction);
+        nodesPendingEviction.clear();
+        return nodesPendingEvictionCopy;
+    }
+
+    private void removeLeastRecentlyUsedNodePathsUntilCanAccommodateSize(long size) {
         Iterator<String> cachedNodesIterator = cachedNodes.keySet().iterator();
         while (((currentSize + size) / ONE_MB >= MAX_SIZE_IN_MB) && cachedNodes.size() > 0) {
             String firstPath = cachedNodesIterator.next();
-            DataCacheRecord recordRemoved = cachedNodes.remove(firstPath);
-            currentSize -= recordRemoved.size;
+            long sizeOfRemovedNode = cachedNodes.remove(firstPath);
+            currentSize -= sizeOfRemovedNode;
+
+            nodesPendingEviction.add(firstPath);
         }
     }
 }
