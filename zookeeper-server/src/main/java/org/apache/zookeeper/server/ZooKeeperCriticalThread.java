9f28747bce0341f974b0ba0d20fd22cfe3343d43
Enrico Olivelli <eolivelli@apache.org>
2019-03-30 07:36:55 +0100

    Fix after merge master

--- zookeeper-server/src/main/java/org/apache/zookeeper/server/ZooKeeperCriticalThread.java
+++ zookeeper-server/src/main/java/org/apache/zookeeper/server/ZooKeeperCriticalThread.java
@@ -47,6 +47,6 @@ public class ZooKeeperCriticalThread extends ZooKeeperThread {
     protected void handleException(String threadName, Throwable e) {
         LOG.error("Severe unrecoverable error, from thread : {}", threadName, e);
         listener.notifyStopping(threadName, ExitCode.UNEXPECTED_ERROR.getValue());
-        ServerMetrics.UNRECOVERABLE_ERROR_COUNT.add(1);
+        ServerMetrics.getMetrics().UNRECOVERABLE_ERROR_COUNT.add(1);
     }
 }
