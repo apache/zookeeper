22e3027f21ba2c95a31ff011a121e7f78d4fb735
Suhas Dantkale <sdantkale@salesforce.com>
2019-06-06 08:47:01 -0700

    Memory Leak. zh->fd which holds the pointer to zsock_t was not getting freed

--- zookeeper-client/zookeeper-client-c/src/zookeeper.c
+++ zookeeper-client/zookeeper-client-c/src/zookeeper.c
@@ -1312,6 +1312,7 @@ static zhandle_t *zookeeper_init_internal(const char *host, watcher_fn watcher,
 abort:
     errnosave=errno;
     destroy(zh);
+    free(zh->fd);
     free(zh);
     errno=errnosave;
     return 0;
@@ -3494,6 +3495,7 @@ int zookeeper_close(zhandle_t *zh)
 finish:
     destroy(zh);
     adaptor_destroy(zh);
+    free(zh->fd);
     free(zh);
 #ifdef _WIN32
     Win32WSACleanup();
