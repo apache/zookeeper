c292ec4dde0cbf2c1578440a9d23ecfb3af8a2de
Brian Nixon <nixon@fb.com>
2019-06-10 15:39:17 -0700

    ZOOKEEPER-3421: Better insight into Observer connections

--- zookeeper-server/src/test/java/org/apache/zookeeper/server/admin/CommandsTest.java
+++ zookeeper-server/src/test/java/org/apache/zookeeper/server/admin/CommandsTest.java
@@ -120,6 +120,17 @@ public class CommandsTest extends ClientBase {
     }
 
     @Test
+    public void testObservers() throws IOException, InterruptedException {
+        testCommand("observers",
+                new Field("synced_observers", Integer.class));
+    }
+
+    @Test
+    public void testObserverConnectionStatReset() throws IOException, InterruptedException {
+        testCommand("observer_connection_stat_reset");
+    }
+
+    @Test
     public void testConnectionStatReset() throws IOException, InterruptedException {
         testCommand("connection_stat_reset");
     }
