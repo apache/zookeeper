ff1923391cb08bd26d460a2faae7c967eaaffa42
Jie Huang <jiehuang@fb.com>
2019-11-13 09:25:52 -0800

    fix spotbugs issue

--- zookeeper-server/src/main/java/org/apache/zookeeper/server/quorum/Learner.java
+++ zookeeper-server/src/main/java/org/apache/zookeeper/server/quorum/Learner.java
@@ -105,7 +105,7 @@ public class Learner {
     private static final boolean nodelay = System.getProperty("follower.nodelay", "true").equals("true");
 
     public static final String LEARNER_ASYNC_SENDING = "learner.asyncSending";
-    public static boolean asyncSending = Boolean.getBoolean(LEARNER_ASYNC_SENDING);
+    protected static boolean asyncSending = Boolean.getBoolean(LEARNER_ASYNC_SENDING);
     static {
         LOG.info("leaderConnectDelayDuringRetryMs: {}", leaderConnectDelayDuringRetryMs);
         LOG.info("TCP NoDelay set to: {}", nodelay);
