d99c9e8b701ebdbd3f0098d4d81696df97432262
tison <wander4096@gmail.com>
2020-05-17 15:13:37 +0200

    ZOOKEEPER-837: Eliminate cycle dependency between ClientCnxn and ZooKeeper
    1. Extract ZKWatchManager to single file
    2. Move ZKWatchManager instance to ClientCnxn in order to eliminate cycle dependency
    3. let `ZooKeeper` syncs a copy of default watcher, in order to reduce dependencies to `getWatchManager()`
    
    Author: tison <wander4096@gmail.com>
    
    Reviewers: Enrico Olivelli <eolivelli@apache.org>, Andor Molnar <andor@apache.org>
    
    Closes #1095 from TisonKun/ZOOKEEPER-837

--- zookeeper-server/src/test/java/org/apache/zookeeper/TestableZooKeeper.java
+++ zookeeper-server/src/test/java/org/apache/zookeeper/TestableZooKeeper.java
@@ -20,12 +20,10 @@ package org.apache.zookeeper;
 
 import java.io.IOException;
 import java.net.SocketAddress;
-import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import org.apache.jute.Record;
 import org.apache.zookeeper.admin.ZooKeeperAdmin;
-import org.apache.zookeeper.client.HostProvider;
 import org.apache.zookeeper.proto.ReplyHeader;
 import org.apache.zookeeper.proto.RequestHeader;
 
@@ -35,76 +33,12 @@ public class TestableZooKeeper extends ZooKeeperAdmin {
         super(host, sessionTimeout, watcher);
     }
 
-    class TestableClientCnxn extends ClientCnxn {
-
-        TestableClientCnxn(
-            String chrootPath,
-            HostProvider hostProvider,
-            int sessionTimeout,
-            ZooKeeper zooKeeper,
-            ClientWatchManager watcher,
-            ClientCnxnSocket clientCnxnSocket,
-            boolean canBeReadOnly) throws IOException {
-            super(chrootPath,
-                  hostProvider,
-                  sessionTimeout,
-                  zooKeeper,
-                  watcher,
-                  clientCnxnSocket,
-                  0,
-                  new byte[16],
-                  canBeReadOnly);
-        }
-
-        void setXid(int newXid) {
-            xid = newXid;
-        }
-
-        int checkXid() {
-            return xid;
-        }
-
-    }
-
-    protected ClientCnxn createConnection(
-        String chrootPath,
-        HostProvider hostProvider,
-        int sessionTimeout,
-        ZooKeeper zooKeeper,
-        ClientWatchManager watcher,
-        ClientCnxnSocket clientCnxnSocket,
-        boolean canBeReadOnly) throws IOException {
-        return new TestableClientCnxn(
-            chrootPath,
-            hostProvider,
-            sessionTimeout,
-            this,
-            watcher,
-            clientCnxnSocket,
-            canBeReadOnly);
-    }
-
     public void setXid(int xid) {
-        ((TestableClientCnxn) cnxn).setXid(xid);
+        cnxn.xid = xid;
     }
 
     public int checkXid() {
-        return ((TestableClientCnxn) cnxn).checkXid();
-    }
-
-    @Override
-    public List<String> getChildWatches() {
-        return super.getChildWatches();
-    }
-
-    @Override
-    public List<String> getDataWatches() {
-        return super.getDataWatches();
-    }
-
-    @Override
-    public List<String> getExistWatches() {
-        return super.getExistWatches();
+        return cnxn.xid;
     }
 
     /**
