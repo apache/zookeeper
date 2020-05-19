017094eb637e8d2bf8e4b797e57af015d16d067c
David Mollitor <dmollitor@apache.org>
2020-05-19 10:02:24 -0400

    ZOOKEEPER-3835: Deprecate IOUtils copyBytes

--- zookeeper-server/src/main/java/org/apache/zookeeper/common/IOUtils.java
+++ zookeeper-server/src/main/java/org/apache/zookeeper/common/IOUtils.java
@@ -78,7 +78,9 @@ public class IOUtils {
      * @param close
      *            whether or not close the InputStream and OutputStream at the
      *            end. The streams are closed in the finally clause.
+     * @deprecated No longer used in ZK
      */
+    @Deprecated
     public static void copyBytes(InputStream in, OutputStream out, int buffSize, boolean close) throws IOException {
         try {
             copyBytes(in, out, buffSize);
