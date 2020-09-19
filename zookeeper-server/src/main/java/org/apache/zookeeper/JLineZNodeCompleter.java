e7852bc509229e7dee9edfc3d678bf72e7276fd3
Suraj <suraj.naikxp@gmail.com>
2020-09-19 16:27:34 +0000

    Update Jline to 3.16.0

--- zookeeper-server/src/main/java/org/apache/zookeeper/JLineZNodeCompleter.java
+++ zookeeper-server/src/main/java/org/apache/zookeeper/JLineZNodeCompleter.java
@@ -20,7 +20,11 @@ package org.apache.zookeeper;
 
 import java.util.Collections;
 import java.util.List;
-import jline.console.completer.Completer;
+
+import org.jline.reader.Candidate;
+import org.jline.reader.Completer;
+import org.jline.reader.LineReader;
+import org.jline.reader.ParsedLine;
 
 class JLineZNodeCompleter implements Completer {
 
@@ -28,6 +32,23 @@ class JLineZNodeCompleter implements Completer {
 
     public JLineZNodeCompleter(ZooKeeper zk) {
         this.zk = zk;
+    }
+
+    @Override
+    public void complete(LineReader lineReader, ParsedLine parsedLine, List<Candidate> list) {
+        String buffer = lineReader.getBuffer().substring(0, parsedLine.cursor());
+        String token = "";
+        if (!buffer.endsWith(" ")) {
+            String[] tokens = buffer.split(" ");
+            if (tokens.length != 0) {
+                token = tokens[tokens.length - 1];
+            }
+        }
+
+        if (token.startsWith("/")) {
+            completeZNode(buffer, token, list);
+        }
+        completeCommand(buffer, token, list);
     }
 
     @SuppressWarnings({"unchecked", "rawtypes"})
@@ -48,31 +69,28 @@ class JLineZNodeCompleter implements Completer {
         return completeCommand(buffer, token, candidates);
     }
 
-    private int completeCommand(String buffer, String token, List<String> candidates) {
+    private int completeCommand(String buffer, String token, List<Candidate> candidates) {
         for (String cmd : ZooKeeperMain.getCommands()) {
             if (cmd.startsWith(token)) {
-                candidates.add(cmd);
+                candidates.add(new Candidate(cmd));
             }
         }
         return buffer.lastIndexOf(" ") + 1;
     }
 
-    private int completeZNode(String buffer, String token, List<String> candidates) {
-        String path = token;
-        int idx = path.lastIndexOf("/") + 1;
-        String prefix = path.substring(idx);
+    private int completeZNode(String buffer, String token, List<Candidate> candidates) {
+        int idx = token.lastIndexOf("/") + 1;
+        String prefix = token.substring(idx);
         try {
             // Only the root path can end in a /, so strip it off every other prefix
-            String dir = idx == 1 ? "/" : path.substring(0, idx - 1);
+            String dir = idx == 1 ? "/" : token.substring(0, idx - 1);
             List<String> children = zk.getChildren(dir, false);
             for (String child : children) {
                 if (child.startsWith(prefix)) {
-                    candidates.add(child);
+                    candidates.add(new Candidate(child));
                 }
             }
-        } catch (InterruptedException e) {
-            return 0;
-        } catch (KeeperException e) {
+        } catch (InterruptedException | KeeperException e) {
             return 0;
         }
         Collections.sort(candidates);
