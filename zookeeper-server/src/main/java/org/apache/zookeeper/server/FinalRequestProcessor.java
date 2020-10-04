0813118d60165c2e3e524af6729a7e8ffabf28a0
Tamas Penzes <tamaas@cloudera.com>
2020-10-04 09:56:25 +0200

    ZOOKEEPER-3952: Remove commons-lang from ZooKeeper
    Change-Id: I798022e64a1ed71ab202a132122adc80a4d88f68

--- zookeeper-server/src/main/java/org/apache/zookeeper/server/FinalRequestProcessor.java
+++ zookeeper-server/src/main/java/org/apache/zookeeper/server/FinalRequestProcessor.java
@@ -25,7 +25,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 import java.util.Set;
-
 import org.apache.jute.Record;
 import org.apache.zookeeper.ClientCnxn;
 import org.apache.zookeeper.KeeperException;
