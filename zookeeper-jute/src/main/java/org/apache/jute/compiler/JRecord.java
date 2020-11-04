7d47a1792170099276c28456eea523d4d1067a6c
David Mollitor <dmollitor@apache.org>
2020-11-04 07:43:20 +0000

    ZOOKEEPER-3688: Use StandardCharsets UTF-8 in Jute toString
    Author: David Mollitor <dmollitor@apache.org>
    
    Reviewers: Christopher Tubbs <ctubbsii@apache.org>, Enrico Olivelli <eolivelli@apache.org>, maoling <maoling199210191@sina.com>, Damien Diederen <ddiederen@apache.org>
    
    Closes #1216 from belugabehr/ZOOKEEPER-3688

--- zookeeper-jute/src/main/java/org/apache/jute/compiler/JRecord.java
+++ zookeeper-jute/src/main/java/org/apache/jute/compiler/JRecord.java
@@ -498,7 +498,7 @@ public class JRecord extends JCompType {
                 jj.write(jf.genJavaWriteMethodName());
             }
             jj.write("      a_.endRecord(this,\"\");\n");
-            jj.write("      return new String(s.toByteArray(), \"UTF-8\");\n");
+            jj.write("      return new String(s.toByteArray(), java.nio.charset.StandardCharsets.UTF_8);\n");
             jj.write("    } catch (Throwable ex) {\n");
             jj.write("      ex.printStackTrace();\n");
             jj.write("    }\n");
