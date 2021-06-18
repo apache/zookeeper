2b49e106c8cafdfe52b634d1665280a10151cba0
zhongxiong.zeng <zhongxiong.zeng@za.group>
2021-06-18 11:09:21 +0800

    Revert "remove redundant code"
    This reverts commit 081c2a03

--- zookeeper-server/src/main/java/org/apache/zookeeper/server/quorum/FastLeaderElection.java
+++ zookeeper-server/src/main/java/org/apache/zookeeper/server/quorum/FastLeaderElection.java
@@ -976,9 +976,25 @@ public class FastLeaderElection implements Election {
                     /*
                      * Exponential backoff
                      */
-                    notTimeout = Math.min(notTimeout<<1, maxNotificationInterval);
+                    notTimeout = Math.min(notTimeout << 1, maxNotificationInterval);
+
+                    /*
+                     * When a leader failure happens on a master, the backup will be supposed to receive the honour from
+                     * Oracle and become a leader, but the honour is likely to be delay. We do a re-check once timeout happens
+                     *
+                     * The leader election algorithm does not provide the ability of electing a leader from a single instance
+                     * which is in a configuration of 2 instances.
+                     * */
+                    if (self.getQuorumVerifier() instanceof QuorumOracleMaj
+                            && self.getQuorumVerifier().revalidateVoteset(voteSet, notTimeout != minNotificationInterval)) {
+                        setPeerState(proposedLeader, voteSet);
+                        Vote endVote = new Vote(proposedLeader, proposedZxid, logicalclock.get(), proposedEpoch);
+                        leaveInstance(endVote);
+                        return endVote;
+                    }
 
                     LOG.info("Notification time out: {} ms", notTimeout);
+
                 } else if (validVoter(n.sid) && validVoter(n.leader)) {
                     /*
                      * Only proceed if the vote comes from a replica in the current or next
