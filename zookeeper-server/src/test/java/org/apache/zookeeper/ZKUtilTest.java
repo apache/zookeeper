f4fb51d97fa0aef4424a094725b8c06bcbfca1aa
Blank <autumind@gmail.com>
2021-06-26 10:06:24 +0800

    Fix unit tests

--- zookeeper-server/src/test/java/org/apache/zookeeper/ZKUtilTest.java
+++ zookeeper-server/src/test/java/org/apache/zookeeper/ZKUtilTest.java
@@ -97,6 +97,6 @@ public class ZKUtilTest extends ClientBase {
         zk.create("/a", "some".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
         zk.create("/a/b", "some".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
         List<String> list = ZKUtil.listSubTreeBFS(zk, "/");
-        assertIterableEquals(list, Arrays.asList("/a", "/a/b"));
+        assertIterableEquals(list, Arrays.asList("/", "/a", "/a/b"));
     }
 }
