c2ab36c9c9f2f6b31f1eee5e8994feafccfe2799
Blank <autumind@gmail.com>
2021-06-29 14:33:22 +0800

    Remove redundant imports

--- zookeeper-server/src/test/java/org/apache/zookeeper/ZKUtilTest.java
+++ zookeeper-server/src/test/java/org/apache/zookeeper/ZKUtilTest.java
@@ -23,7 +23,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
