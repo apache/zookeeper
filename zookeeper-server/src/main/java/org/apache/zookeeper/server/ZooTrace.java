39279c78afaff9d0d9a154de82e2010fe263bdc7
zhongxiong.zeng <zhongxiong.zeng@za.group>
2021-07-01 11:23:37 +0800

    remove never used field

--- zookeeper-server/src/main/java/org/apache/zookeeper/server/ZooTrace.java
+++ zookeeper-server/src/main/java/org/apache/zookeeper/server/ZooTrace.java
@@ -33,8 +33,6 @@ public class ZooTrace {
 
     public static final long CLIENT_REQUEST_TRACE_MASK = 1 << 1;
 
-    public static final long CLIENT_DATA_PACKET_TRACE_MASK = 1 << 2;
-
     public static final long CLIENT_PING_TRACE_MASK = 1 << 3;
 
     public static final long SERVER_PACKET_TRACE_MASK = 1 << 4;
@@ -46,8 +44,6 @@ public class ZooTrace {
     public static final long SERVER_PING_TRACE_MASK = 1 << 7;
 
     public static final long WARNING_TRACE_MASK = 1 << 8;
-
-    public static final long JMX_TRACE_MASK = 1 << 9;
 
     private static long traceMask = CLIENT_REQUEST_TRACE_MASK | SERVER_PACKET_TRACE_MASK | SESSION_TRACE_MASK | WARNING_TRACE_MASK;
 
