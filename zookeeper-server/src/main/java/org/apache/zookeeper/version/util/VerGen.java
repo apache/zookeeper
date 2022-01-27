c045747dd3c5ff3ab68629a55b4b534db7982420
maoling <maoling@apache.org>
2022-01-27 18:43:14 +0800

    ZOOKEEPER-3423: use the maven-like way to ignore the generated version java files

--- zookeeper-server/src/main/java/org/apache/zookeeper/version/util/VerGen.java
+++ zookeeper-server/src/main/java/org/apache/zookeeper/version/util/VerGen.java
@@ -30,7 +30,7 @@ public class VerGen {
 
     static void printUsage() {
         System.out.print("Usage:\tjava  -cp <classpath> org.apache.zookeeper."
-                + "version.util.VerGen maj.min.micro[-qualifier] rev buildDate");
+                + "version.util.VerGen maj.min.micro[-qualifier] rev buildDate outputDirectory");
         System.exit(1);
     }
 
@@ -141,7 +141,7 @@ public class VerGen {
      *            </ul>
      */
     public static void main(String[] args) {
-        if (args.length != 3)
+        if (args.length != 4)
             printUsage();
         try {
             Version version = parseVersionString(args[0]);
@@ -156,7 +156,7 @@ public class VerGen {
             } else {
                 rev = rev.trim();
             }
-            generateFile(new File("."), version, rev, args[2]);
+            generateFile(new File(args[3]), version, rev, args[2]);
         } catch (NumberFormatException e) {
             System.err.println(
                     "All version-related parameters must be valid integers!");
