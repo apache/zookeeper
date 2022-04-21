a041a26c3eb12a8fd520a1c5518707f3a61c9f3c
Rodrigo Nicolas Garcia <rodrigo.garcia@despegar.com>
2022-04-21 08:29:06 -0300

    Update PrepRequestProcessor.java
    change error to warn

--- zookeeper-server/src/main/java/org/apache/zookeeper/server/PrepRequestProcessor.java
+++ zookeeper-server/src/main/java/org/apache/zookeeper/server/PrepRequestProcessor.java
@@ -419,7 +419,7 @@ public class PrepRequestProcessor extends ZooKeeperCriticalThread implements Req
             try {
                 lzks = (LeaderZooKeeperServer) zks;
             } catch (ClassCastException e) {
-                LOG.error("Reconfig operation requested but this feature is currently not supported on standalone mode.");
+                LOG.warn("Reconfig operation requested but this feature is currently not supported on standalone mode.");
                 throw new KeeperException.UnimplementedException();
             }
             QuorumVerifier lastSeenQV = lzks.self.getLastSeenQuorumVerifier();
