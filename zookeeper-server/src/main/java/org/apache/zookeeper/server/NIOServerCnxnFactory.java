51cbc404584a16167194469e88254478f0fd2046
estellahuang <huangyuxincs@163.com>
2022-06-02 14:54:25 -0700

    change check style

--- zookeeper-server/src/main/java/org/apache/zookeeper/server/NIOServerCnxnFactory.java
+++ zookeeper-server/src/main/java/org/apache/zookeeper/server/NIOServerCnxnFactory.java
@@ -19,10 +19,10 @@
 package org.apache.zookeeper.server;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import org.apache.zookeeper.common.StringConvertUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.SocketException;
