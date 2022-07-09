022c17430e0645e890c14fda22738389411175b5
tison <wander4096@gmail.com>
2022-07-09 13:40:09 +0800

    fix checkstyle
    Signed-off-by: tison <wander4096@gmail.com>

--- zookeeper-server/src/test/java/org/apache/zookeeper/server/CreateContainerTest.java
+++ zookeeper-server/src/test/java/org/apache/zookeeper/server/CreateContainerTest.java
@@ -27,7 +27,6 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.Callable;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
