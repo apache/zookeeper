85619766e84ce6e5e23773d46506a8d5ea0ba2ba
zouyonghao <yonghaoz1994@gmail.com>
2023-06-08 00:47:47 +0800

    Make closeSockSync synchronized to avoid NPE

--- zookeeper-server/src/main/java/org/apache/zookeeper/server/quorum/Learner.java
+++ zookeeper-server/src/main/java/org/apache/zookeeper/server/quorum/Learner.java
@@ -892,7 +892,7 @@ public class Learner {
         }
     }
 
-    void closeSockSync() {
+    synchronized void closeSockSync() {
         try {
             long startTime = Time.currentElapsedTime();
             if (sock != null) {
