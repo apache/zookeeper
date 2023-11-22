1816aec8234c9b3de1a7dd87b09868b4dc48d854
Shawyeok <kmter@live.com>
2023-11-22 21:49:14 +0800

    Address pr comments

--- zookeeper-metrics-providers/zookeeper-prometheus-metrics/src/main/java/org/apache/zookeeper/metrics/prometheus/PrometheusMetricsProvider.java
+++ zookeeper-metrics-providers/zookeeper-prometheus-metrics/src/main/java/org/apache/zookeeper/metrics/prometheus/PrometheusMetricsProvider.java
@@ -72,6 +72,7 @@ public class PrometheusMetricsProvider implements MetricsProvider {
      * Number of worker threads for reporting Prometheus summary metrics.
      * Default value is 1.
      * If the number is less than 1, the main thread will be used.
+     * @deprecated This configuration is ignored. See details in ZOOKEEPER-4741
      */
     @Deprecated
     static final String NUM_WORKER_THREADS = "numWorkerThreads";
@@ -79,6 +80,7 @@ public class PrometheusMetricsProvider implements MetricsProvider {
     /**
      * The max queue size for Prometheus summary metrics reporting task.
      * Default value is 1000000.
+     * @deprecated This configuration is ignored. See details in ZOOKEEPER-4741
      */
     @Deprecated
     static final String MAX_QUEUE_SIZE = "maxQueueSize";
