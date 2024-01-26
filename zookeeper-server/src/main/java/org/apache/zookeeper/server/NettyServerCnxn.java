1acb7e02e8a812e56027d869eaff8f2e0207eb73
Maxi Wittich <maximilian.wittich@stackable.tech>
2024-01-26 13:27:38 +0100

    now unknown

--- zookeeper-server/src/main/java/org/apache/zookeeper/server/NettyServerCnxn.java
+++ zookeeper-server/src/main/java/org/apache/zookeeper/server/NettyServerCnxn.java
@@ -528,7 +528,7 @@ public class NettyServerCnxn extends ServerCnxn {
                         ZooKeeperServer zks = this.zkServer;
                         if (zks == null || !zks.isRunning()) {
                             LOG.info("Closing connection to {} because the server is not ready (server state is: {})",
-                                getRemoteSocketAddress(), zks == null ? "UNKNOWN" : zks.getState());
+                                getRemoteSocketAddress(), zks == null ? "unknown" : zks.getState());
                             close(DisconnectReason.IO_EXCEPTION);
                             return;
                         }
