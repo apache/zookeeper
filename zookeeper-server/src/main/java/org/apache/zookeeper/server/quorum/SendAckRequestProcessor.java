8085a3514e4d2c5d8792fa679175a19cfa3e4025
Jon Marius Venstad <jonmv@users.noreply.github.com>
2024-04-10 16:23:00 +0200

    Remove extra newline in imports

--- zookeeper-server/src/main/java/org/apache/zookeeper/server/quorum/SendAckRequestProcessor.java
+++ zookeeper-server/src/main/java/org/apache/zookeeper/server/quorum/SendAckRequestProcessor.java
@@ -21,7 +21,6 @@ package org.apache.zookeeper.server.quorum;
 import java.io.Flushable;
 import java.io.IOException;
 import java.net.Socket;
-
 import org.apache.zookeeper.ZooDefs.OpCode;
 import org.apache.zookeeper.server.Request;
 import org.apache.zookeeper.server.RequestProcessor;
