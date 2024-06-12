30c28972afa6511a6142b43c4efc3b44052d3fd0
mujtaba1747 <mujtaba1747@gmail.com>
2024-06-13 01:48:02 +0530

    WIP: create API: Added classes for sending SQS msg

--- zookeeper-server/src/main/java/org/apache/zookeeper/faaskeeper/AwsClient.java
+++ zookeeper-server/src/main/java/org/apache/zookeeper/faaskeeper/AwsClient.java
@@ -1,4 +1,5 @@
 package org.apache.zookeeper.faaskeeper;
+// ddb deps
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
@@ -7,20 +8,44 @@ import software.amazon.awssdk.services.dynamodb.model.PutItemRequest;
 import software.amazon.awssdk.services.dynamodb.model.PutItemResponse;
 import software.amazon.awssdk.services.dynamodb.model.ResourceNotFoundException;
 
+// sqs deps
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import com.amazonaws.services.sqs.model.SendMessageRequest;
+import com.amazonaws.services.sqs.model.SendMessageResult;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AwsClient extends ProviderClient {
-    private DynamoDbClient ddb;
-    private String userTable;
+    private final AmazonSQS sqs;
+    private final DynamoDbClient ddb;
+    private final String userTable;
+    private static final Logger LOG;
+    private final String writerQueueName;
+    private final String writerQueueUrl;
+    static {
+        LOG = LoggerFactory.getLogger(AwsClient.class);
+    }
     
     public AwsClient(FaasKeeperConfig config) {
         super(config);
+
         this.ddb = DynamoDbClient.builder()
             .region(Region.of(config.getDeploymentRegion()))
             .build();
+        this.sqs = AmazonSQSClientBuilder.standard()
+            .withRegion(config.getDeploymentRegion()) // Specify the desired AWS region
+            .build();
+        
         this.userTable = String.format("faaskeeper-%s-users", config.getDeploymentName());
+        this.writerQueueName = String.format("faaskeeper-%s-writer-sqs.fifo", config.getDeploymentName());
+        this.writerQueueUrl = sqs.getQueueUrl(writerQueueName).getQueueUrl();
     }
 
     public void registerSession(String sessionId, String sourceAddr, boolean heartbeat) {
@@ -33,16 +58,41 @@ public class AwsClient extends ProviderClient {
             .item(itemValues)
             .build();
 
-        // TODO: Add Logger for this
         try {
             PutItemResponse response = ddb.putItem(request);
-            System.out.println(userTable + " was successfully updated. The request id is " + response.responseMetadata().requestId());
+            LOG.info(userTable + " was successfully updated. The request id is " + response.responseMetadata().requestId());
 
         } catch (ResourceNotFoundException e) {
-            System.err.format("Error: The Amazon DynamoDB table \"%s\" can't be found.\n", userTable);
-            System.err.println("Be sure that it exists and that you've typed its name correctly!");
+            LOG.error(String.format("Error: The Amazon DynamoDB table \"%s\" can't be found.\n", userTable));
         } catch (DynamoDbException e) {
             System.err.println(e.getMessage());
         }
     }
+
+    public void sendRequest(String requestId, Map <String, Object> data) throws Exception {
+        try {
+            if (config.getWriterQueue() == QueueType.SQS) {
+                ObjectMapper objectMapper = new ObjectMapper();
+                String serializedData = objectMapper.writeValueAsString(data);
+                LOG.info("Attempting to send msg: " + serializedData);
+
+                SendMessageRequest send_msg_request = new SendMessageRequest()
+                    .withMessageGroupId("0")
+                    .withMessageDeduplicationId(requestId)
+                    .withQueueUrl(writerQueueUrl)
+                    .withMessageBody(serializedData);
+                
+                SendMessageResult res = sqs.sendMessage(send_msg_request);
+                LOG.debug("Msg sent successfully: " + res.toString());
+            } else {
+                throw new UnsupportedOperationException("Unsupported queue type specified in sendRequest: " + config.getWriterQueue().name());
+            }
+
+        } catch(Exception e) {
+            LOG.error("Error sending request: ", e);
+            throw e;
+        }
+    }
+
+
 }
---
+++ zookeeper-server/src/main/java/org/apache/zookeeper/faaskeeper/DynamoReader.java
@@ -0,0 +1,56 @@
+package org.apache.zookeeper.faaskeeper;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Base64;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DynamoReader {
+    private static final Logger LOG;
+    static {
+        LOG = LoggerFactory.getLogger(DynamoReader.class);
+    }
+
+    // Method to determine the DynamoDB type of a value
+    private static String dynamoDBType(Object value) {
+        if (value instanceof String) {
+            return "S";
+        } else if (value instanceof Integer) {
+            return "N";
+        } else if (value instanceof byte[]) {
+            return "B";
+        } else if (value instanceof Object[]) {
+            return "L";
+        } else {
+            throw new IllegalArgumentException("Unsupported data type for DynamoDB: " + value.getClass().getSimpleName());
+        }
+    }
+
+    // TODO: Implement serialization for []Object too if needed
+    // Method to convert the value to a suitable format for DynamoDB
+    private static String dynamoDBValue(Object value) {
+        if (value instanceof byte[]) {
+            return Base64.getEncoder().encodeToString((byte[]) value);
+        } else if (value instanceof String){
+            return (String) value;
+        } else {
+            LOG.error("Unhandled datatype in dynamoDBValue. Default toString() value will be returned");
+            return value.toString();
+        }
+    }
+
+    // Method to convert items to a DynamoDB compatible format
+    public static Map<String, Map<String, Object>> convertItems(Map<String, Object> items) {
+        Map<String, Map<String, Object>> convertedItems = new HashMap<>();
+        for (Map.Entry<String, Object> entry : items.entrySet()) {
+            String key = entry.getKey();
+            Object value = entry.getValue();
+            String type = dynamoDBType(value);
+            Object formattedValue = dynamoDBValue(value);
+            Map<String, Object> valueMap = new HashMap<>();
+            valueMap.put(type, formattedValue);
+            convertedItems.put(key, valueMap);
+        }
+        return convertedItems;
+    }
+}
