7236a293c1ae083f136eae0c3d75aea3af396877
Andrew-Chedid <andrew.chedid.1@ens.etsmtl.ca>
2025-03-14 13:13:06 -0400

    test changes in Java project

--- zookeeper-server/src/main/java/org/apache/zookeeper/CreateOptions.java
+++ zookeeper-server/src/main/java/org/apache/zookeeper/CreateOptions.java
@@ -74,6 +74,7 @@ public class CreateOptions {
         private Builder(CreateMode createMode, List<ACL> acl) {
             this.createMode = Objects.requireNonNull(createMode, "create mode is mandatory for create options");
             this.acl = Objects.requireNonNull(acl, "acl is mandatory for create options");
+            System.out.println("testing world");
         }
 
         public Builder withTtl(long ttl) {
