9bdf91dc1df5c36ff5b29e9a6e156f9bec7dfa5a
Kezhu Wang <kezhuw@apache.org>
2025-04-20 21:45:58 +0800

    turn of FIPS mode to use DIGEST-MD5 SASL

--- zookeeper-server/src/test/java/org/apache/zookeeper/server/quorum/auth/QuorumAuthObserverTest.java
+++ zookeeper-server/src/test/java/org/apache/zookeeper/server/quorum/auth/QuorumAuthObserverTest.java
@@ -29,7 +29,7 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
-public class QuorumAuthObserverTest extends QuorumAuthTestBase {
+public class QuorumAuthObserverTest extends DigestSecurityTestcase {
 
     static {
         String jaasEntries = "QuorumServer {\n"
