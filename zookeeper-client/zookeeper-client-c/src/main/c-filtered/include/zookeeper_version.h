b52ad0a152c2b7c4086b82ac4bdbb62f345fb7a0
Christopher Tubbs <ctubbsii@apache.org>
2025-05-08 15:57:22 -0400

    Remove and untrack generated files
    * Remove generated files
    * Add generated files to .gitignore
    * Add a comment to the generated files so developers know which files to
      edit

--- zookeeper-client/zookeeper-client-c/src/main/c-filtered/include/zookeeper_version.h
+++ zookeeper-client/zookeeper-client-c/src/main/c-filtered/include/zookeeper_version.h
@@ -15,6 +15,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+/* @filteredBuildComment@ */
+
 #ifndef ZOOKEEPER_VERSION_H_
 #define ZOOKEEPER_VERSION_H_
 
