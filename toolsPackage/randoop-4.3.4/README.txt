f82c0198420faf80a114601b44b7cd2af58b11db
Lucacupellaro <luca.cupellaro@students.uniroma2.eu>
2026-04-15 22:16:02 +0200

    randoop e classi scelte

---
+++ toolsPackage/randoop-4.3.4/README.txt
@@ -0,0 +1,2 @@
+To get started with this Randoop distribution, see the user manual in
+doc/manual/index.html or online at https://randoop.github.io/randoop/manual/index.html.
---
+++ zookeeper-server/src/test/java/org/apache/zookeeper/randoop/RegressionTest.java
@@ -0,0 +1,10 @@
+package org.apache.zookeeper.randoop;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ RegressionTest0.class })
+public class RegressionTest {
+}
+
