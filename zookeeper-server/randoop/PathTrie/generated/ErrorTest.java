5c4d94980b936dac2608765528f357da3c7ead37
Lucacupellaro <luca.cupellaro@students.uniroma2.eu>
2026-05-13 21:33:32 +0200

    concluso evosuite

---
+++ zookeeper-server/randoop/PathTrie/generated/ErrorTest.java
@@ -0,0 +1,8 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ ErrorTest0.class })
+public class ErrorTest {
+}
+
---
+++ zookeeper-server/src/test/java/PathTrieTest/randoop/RegressionTest.java
@@ -0,0 +1,10 @@
+package PathTrieTest.randoop;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ RegressionTest0.class })
+public class RegressionTest {
+}
+
